4938cb306dbaa28279025996cbecb0dd7ebe086e
Zeyla Hellyer <zeyla@hellyer.dev>
2019-09-03 19:37:40 +0100

    model: channel/message/reference: fix 'channel_id'
    Fix `channel::message::MessageReference::channel_id` to make it
    required, not optional.
    
    Signed-off-by: Zeyla Hellyer <zeyla@hellyer.dev>

--- model/src/channel/message/reference.rs
+++ model/src/channel/message/reference.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct MessageReference {
-    pub channel_id: Option<ChannelId>,
+    pub channel_id: ChannelId,
     pub guild_id: Option<GuildId>,
     pub message_id: Option<MessageId>,
 }
