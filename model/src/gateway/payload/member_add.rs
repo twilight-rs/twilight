9e57c4ad10547601b945d68536662f5532a9dbf3
zeyla hellyer <zeyla@hellyer.dev>
2019-10-16 14:35:35 +0100

    [model] make gateway::payload::memberadd a newtype (#18)
    Make `gateway::payload::MemberAdd` a newtype around `guild::Member`,
    because the payload includes no additional information.
    
    Add a `std::ops::Deref` and `std::ops::DerefMut` implementation on it
    since it's just a newtype around `guild::Member`.
    
    A test case for this event has been added with one of the payloads that
    failed deserialization.
    
    Additionally, update the cache since it uses `MemberAdd`.
    
    Signed-off-by: Zeyla Hellyer <zeyla@hellyer.dev>

--- model/src/gateway/payload/member_add.rs
+++ model/src/gateway/payload/member_add.rs
@@ -1,11 +1,110 @@
-use crate::{guild::Member, id::GuildId};
+use crate::guild::Member;
+use std::ops::{Deref, DerefMut};
 
 #[cfg_attr(
     feature = "serde-support",
     derive(serde::Deserialize, serde::Serialize)
 )]
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub struct MemberAdd {
-    pub guild_id: GuildId,
-    pub member: Member,
+pub struct MemberAdd(pub Member);
+
+impl Deref for MemberAdd {
+    type Target = Member;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for MemberAdd {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Member, MemberAdd};
+    use crate::{
+        id::{GuildId, UserId},
+        user::User,
+    };
+    use serde_test::Token;
+
+    #[test]
+    fn test_member_add() {
+        let member_add = MemberAdd(Member {
+            deaf: false,
+            guild_id: Some(GuildId(1)),
+            hoisted_role: None,
+            joined_at: None,
+            mute: false,
+            nick: None,
+            premium_since: None,
+            roles: vec![],
+            user: User {
+                id: UserId(2),
+                avatar: None,
+                bot: false,
+                discriminator: "0987".to_string(),
+                name: "ab".to_string(),
+            },
+        });
+
+        serde_test::assert_tokens(
+            &member_add,
+            &[
+                Token::NewtypeStruct {
+                    name: "MemberAdd",
+                },
+                Token::Struct {
+                    name: "Member",
+                    len: 9,
+                },
+                Token::Str("deaf"),
+                Token::Bool(false),
+                Token::Str("guild_id"),
+                Token::Some,
+                Token::NewtypeStruct {
+                    name: "GuildId",
+                },
+                Token::Str("1"),
+                Token::Str("hoisted_role"),
+                Token::None,
+                Token::Str("joined_at"),
+                Token::None,
+                Token::Str("mute"),
+                Token::Bool(false),
+                Token::Str("nick"),
+                Token::None,
+                Token::Str("premium_since"),
+                Token::None,
+                Token::Str("roles"),
+                Token::Seq {
+                    len: Some(0),
+                },
+                Token::SeqEnd,
+                Token::Str("user"),
+                Token::Struct {
+                    name: "User",
+                    len: 5,
+                },
+                Token::Str("id"),
+                Token::NewtypeStruct {
+                    name: "UserId",
+                },
+                Token::Str("2"),
+                Token::Str("avatar"),
+                Token::None,
+                Token::Str("bot"),
+                Token::Bool(false),
+                Token::Str("discriminator"),
+                Token::Str("0987"),
+                Token::Str("username"),
+                Token::Str("ab"),
+                Token::StructEnd,
+                Token::StructEnd,
+            ],
+        );
+    }
 }
