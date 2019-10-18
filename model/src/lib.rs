bc78b9ddb1f568a11a69b12147a80c95ffec33e2
Zeyla Hellyer <zeyla@hellyer.dev>
2019-10-18 15:38:13 +0100

    [ci] fix workspace use in actions (#21)
    Workspaces don't support `--all-features` in commands like `cargo test`,
    which is a silent error and ignored, but fails our CI. To fix this,
    remove the flag.
    
    Additionally, switch out the deprecated `--all` for the new
    `--workspace` flag, and remove the `cargo check` action since it's
    covered by the `cargo test` action.
    
    Ignore the `must_use_candidate` Clippy lint, which is a little *too*
    pedantic for us.
    
    This was added a few days ago here:
    <https://github.com/rust-lang/rust-clippy/commit/cc622608db7318b1c0fe3ccd541558436c7c6c4c>
    
    Signed-off-by: Zeyla Hellyer <zeyla@hellyer.dev>

--- model/src/lib.rs
+++ model/src/lib.rs
@@ -72,7 +72,7 @@
     unused,
     warnings
 )]
-#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::module_name_repetitions, clippy::must_use_candidate)]
 
 pub mod channel;
 pub mod gateway;
