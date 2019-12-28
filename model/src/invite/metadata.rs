292a56c15c03ff8fab069183d728eec74257e546
Valdemar Erk <valdemar@erk.io>
2019-12-28 20:08:48 +0100

    [model/invite] Move the field `inviter` to the main struct
    cf. https://github.com/discordapp/discord-api-docs/pull/1270
    
    Signed-off-by: Valdemar Erk <valdemar@erk.io>

--- model/src/invite/metadata.rs
+++ model/src/invite/metadata.rs
@@ -1,5 +1,3 @@
-use crate::user::User;
-
 #[cfg_attr(
     feature = "serde-support",
     derive(serde::Deserialize, serde::Serialize)
@@ -7,7 +5,6 @@ use crate::user::User;
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct InviteMetadata {
     pub created_at: String,
-    pub inviter: User,
     pub max_age: u64,
     pub max_uses: u64,
     pub temporary: bool,
