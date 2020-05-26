3748a6afcf6ce2bbe843adcfb3c5b0e4e1e5d882
Vivian Hellyer <vivian@hellyer.dev>
2020-05-25 22:07:21 -0400

    cargo clippy
    Signed-off-by: Vivian Hellyer <vivian@hellyer.dev>

--- http/src/request/channel/update_channel.rs
+++ http/src/request/channel/update_channel.rs
@@ -174,7 +174,7 @@ impl<'a> UpdateChannel<'a> {
             return Err(UpdateChannelError::TopicInvalid);
         }
 
-        self.fields.topic.replace(topic.into());
+        self.fields.topic.replace(topic);
 
         Ok(self)
     }
