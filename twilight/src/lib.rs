60de1d42a772bb3c10a5a0db0a9ae256af49cac6
Vivian Hellyer <vivian@hellyer.dev>
2020-06-09 21:15:14 -0400

    use project code snippet in main crate's docs
    Use the code snippet from the original project README.md in the twilight
    crate's doc's snippet.
    
    Signed-off-by: Vivian Hellyer <vivian@hellyer.dev>

--- twilight/src/lib.rs
+++ twilight/src/lib.rs
@@ -96,25 +96,63 @@
 //! ## Examples
 //!
 //! ```rust,no_run
-//! use twilight::{
-//!     gateway::{Cluster, ClusterConfig, Event},
-//!     http::Client as HttpClient,
-//! };
-//! use futures::StreamExt;
 //! use std::{env, error::Error};
+//! use tokio::stream::StreamExt;
+//!
+//! use twilight::{
+//!     cache::{
+//!         twilight_cache_inmemory::config::{InMemoryConfigBuilder, EventType},
+//!         InMemoryCache,
+//!     },
+//!     gateway::cluster::{config::ShardScheme, Cluster, ClusterConfig},
+//!     gateway::shard::Event,
+//!     http::Client as HttpClient,
+//!     model::gateway::GatewayIntents,
+//! };
 //!
 //! #[tokio::main]
 //! async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
 //!     let token = env::var("DISCORD_TOKEN")?;
-//!     let http = HttpClient::new(&token);
 //!
-//!     let cluster_config = ClusterConfig::builder(&token).build();
-//!     let cluster = Cluster::new(cluster_config);
+//!     // This is also the default.
+//!     let scheme = ShardScheme::Auto;
+//!
+//!     let config = ClusterConfig::builder(&token)
+//!         .shard_scheme(scheme)
+//!         // Use intents to only listen to GUILD_MESSAGES events
+//!         .intents(Some(
+//!             GatewayIntents::GUILD_MESSAGES | GatewayIntents::DIRECT_MESSAGES,
+//!         ))
+//!         .build();
+//!
+//!     // Start up the cluster
+//!     let cluster = Cluster::new(config);
 //!     cluster.up().await?;
 //!
-//!     let mut events = cluster.events().await;
+//!     // The http client is seperate from the gateway,
+//!     // so startup a new one
+//!     let http = HttpClient::new(&token);
 //!
+//!     // Since we only care about messages, make the cache only
+//!     // cache message related events
+//!     let cache_config = InMemoryConfigBuilder::new()
+//!         .event_types(
+//!             EventType::MESSAGE_CREATE
+//!                 | EventType::MESSAGE_DELETE
+//!                 | EventType::MESSAGE_DELETE_BULK
+//!                 | EventType::MESSAGE_UPDATE,
+//!         )
+//!         .build();
+//!     let cache = InMemoryCache::from(cache_config);
+//!
+//!
+//!     let mut events = cluster.events().await;
+//!     // Startup an event loop for each event in the event stream
 //!     while let Some(event) = events.next().await {
+//!         // Update the cache
+//!         cache.update(&event.1).await.expect("Cache failed, OhNoe");
+//!
+//!         // Spawn a new task to handle the event
 //!         tokio::spawn(handle_event(event, http.clone()));
 //!     }
 //!
@@ -126,13 +164,11 @@
 //!     http: HttpClient,
 //! ) -> Result<(), Box<dyn Error + Send + Sync>> {
 //!     match event {
-//!         (id, Event::Ready(_)) => {
-//!             println!("Connected on shard {}", id);
+//!         (_, Event::MessageCreate(msg)) if msg.content == "!ping" => {
+//!             http.create_message(msg.channel_id).content("Pong!").await?;
 //!         }
-//!         (_, Event::MessageCreate(msg)) => {
-//!             if msg.content == "!ping" {
-//!                 http.create_message(msg.channel_id).content("Pong!")?.await?;
-//!             }
+//!         (id, Event::ShardConnected(_)) => {
+//!             println!("Connected on shard {}", id);
 //!         }
 //!         _ => {}
 //!     }
