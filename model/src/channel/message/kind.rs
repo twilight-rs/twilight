2424f8249c273b27730157db1c6a3343060de767
BlackHoleFox <blackholefoxdev@gmail.com>
2020-07-28 23:02:10 -0500

    Add TryFrom u8 for MessageType

--- model/src/channel/message/kind.rs
+++ model/src/channel/message/kind.rs
@@ -1,3 +1,5 @@
+use crate::channel::ConversionError;
+use core::convert::TryFrom;
 use serde_repr::{Deserialize_repr, Serialize_repr};
 
 #[derive(
@@ -20,4 +22,31 @@ pub enum MessageType {
     ChannelFollowAdd = 12,
     GuildDiscoveryDisqualified = 14,
     GuildDiscoveryRequalified = 15,
+}
+
+impl TryFrom<u8> for MessageType {
+    type Error = ConversionError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        let message_type = match value {
+            0 => MessageType::Regular,
+            1 => MessageType::RecipientAdd,
+            2 => MessageType::RecipientRemove,
+            3 => MessageType::Call,
+            4 => MessageType::ChannelNameChange,
+            5 => MessageType::ChannelIconChange,
+            6 => MessageType::ChannelMessagePinned,
+            7 => MessageType::GuildMemberJoin,
+            8 => MessageType::UserPremiumSub,
+            9 => MessageType::UserPremiumSubTier1,
+            10 => MessageType::UserPremiumSubTier2,
+            11 => MessageType::UserPremiumSubTier3,
+            12 => MessageType::ChannelFollowAdd,
+            14 => MessageType::GuildDiscoveryDisqualified,
+            15 => MessageType::GuildDiscoveryRequalified,
+            _ => return Err(ConversionError::MessageType(value)),
+        };
+
+        Ok(message_type)
+    }
 }
