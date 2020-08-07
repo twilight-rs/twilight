e648f743c2d6a28d313340c6e4042e863528970b
Valdemar Erk <valdemar@erk.io>
2020-08-07 09:27:10 +0200

    [model] Guild member update now always contains joined_at
    cf. https://github.com/discord/discord-api-docs/pull/1858
    
    Signed-off-by: Valdemar Erk <valdemar@erk.io>

--- model/src/gateway/payload/member_update.rs
+++ model/src/gateway/payload/member_update.rs
@@ -7,6 +7,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub struct MemberUpdate {
     pub guild_id: GuildId,
+    pub joined_at: String,
     pub nick: Option<String>,
     pub premium_since: Option<String>,
     pub roles: Vec<RoleId>,
