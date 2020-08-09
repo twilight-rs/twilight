b0b63b707e141dba8f984fce851b09fa4d28dbfa
Vivian <vivian@hellyer.dev>
2020-08-09 14:31:47 -0400

    model/guild: add tests for a few types (#378)
    Add `serde_test` unit tests for the following `guild` module types:
    
    - Ban
    - Emoji
    - GuildPrune
    - GuildWidget
    
    This is a part of #30.
    
    Approved-by: BlackHoleFox <blackholefoxdev@gmail.com>
    Approved-by: Cassandra McCarthy <cassie@7596ff.com>
    Merged-by: Cassandra McCarthy <cassie@7596ff.com>
    Signed-off-by: Vivian Hellyer <vivian@hellyer.dev>

--- model/src/guild/widget.rs
+++ model/src/guild/widget.rs
@@ -6,3 +6,33 @@ pub struct GuildWidget {
     pub channel_id: ChannelId,
     pub enabled: bool,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::{ChannelId, GuildWidget};
+    use serde_test::Token;
+
+    #[test]
+    fn test_guild_widget() {
+        let prune = GuildWidget {
+            channel_id: ChannelId(111_111_111_111_111_111),
+            enabled: true,
+        };
+
+        serde_test::assert_tokens(
+            &prune,
+            &[
+                Token::Struct {
+                    name: "GuildWidget",
+                    len: 2,
+                },
+                Token::Str("channel_id"),
+                Token::NewtypeStruct { name: "ChannelId" },
+                Token::Str("111111111111111111"),
+                Token::Str("enabled"),
+                Token::Bool(true),
+                Token::StructEnd,
+            ],
+        );
+    }
+}
