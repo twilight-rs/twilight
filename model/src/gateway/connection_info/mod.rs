daf585616ca870a858c26b546a159575369f5d0c
Vivian <vivian@hellyer.dev>
2020-08-09 14:31:29 -0400

    model/gateway: add 'max_concurrency' field (#377)
    Add `max_concurrency` to the session start limit struct, which indicates
    how many shard sessions may concurrently be started.
    
    Documentation and tests have been added to go with it.
    
    Approved-by: BlackHoleFox <blackholefoxdev@gmail.com>
    Approved-by: Cassandra McCarthy <cassie@7596ff.com>
    Merged-by: Cassandra McCarthy <cassie@7596ff.com>
    Signed-off-by: Vivian Hellyer <vivian@hellyer.dev>

--- model/src/gateway/connection_info/mod.rs
+++ model/src/gateway/connection_info/mod.rs
@@ -4,7 +4,35 @@ pub use self::bot_connection_info::BotConnectionInfo;
 
 use serde::{Deserialize, Serialize};
 
+/// Gateway information containing the URL to connect to.
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub struct ConnectionInfo {
+    /// URL to the gateway.
     pub url: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ConnectionInfo;
+    use serde_test::Token;
+
+    #[test]
+    fn test_connection_info() {
+        let info = ConnectionInfo {
+            url: "wss://gateway.discord.gg".to_owned(),
+        };
+
+        serde_test::assert_tokens(
+            &info,
+            &[
+                Token::Struct {
+                    name: "ConnectionInfo",
+                    len: 1,
+                },
+                Token::Str("url"),
+                Token::Str("wss://gateway.discord.gg"),
+                Token::StructEnd,
+            ],
+        );
+    }
 }
