df5d3aa9adf06a3bb245db2315f4cf584e0b84ef
Vivian Hellyer <vivian@hellyer.dev>
2020-08-16 12:28:16 -0400

    model/partial_member: add missing 'nick' field
    Add the missing `nick` field to the
    `twilight_model::guild::PartialMember` type.
    
    Member documentation can be found here:
    
    <https://discord.com/developers/docs/resources/guild#guild-member-object-guild-member-structure>
    
    Signed-off-by: Vivian Hellyer <vivian@hellyer.dev>

--- model/src/guild/partial_member.rs
+++ model/src/guild/partial_member.rs
@@ -6,5 +6,51 @@ pub struct PartialMember {
     pub deaf: bool,
     pub joined_at: Option<String>,
     pub mute: bool,
+    pub nick: Option<String>,
     pub roles: Vec<RoleId>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{PartialMember, RoleId};
+    use serde_test::Token;
+
+    #[test]
+    fn test_partial_member() {
+        let value = PartialMember {
+            deaf: true,
+            joined_at: Some("timestamp".to_owned()),
+            mute: true,
+            nick: Some("a nickname".to_owned()),
+            roles: vec![RoleId(1), RoleId(2)],
+        };
+
+        serde_test::assert_tokens(
+            &value,
+            &[
+                Token::Struct {
+                    name: "PartialMember",
+                    len: 5,
+                },
+                Token::Str("deaf"),
+                Token::Bool(true),
+                Token::Str("joined_at"),
+                Token::Some,
+                Token::Str("timestamp"),
+                Token::Str("mute"),
+                Token::Bool(true),
+                Token::Str("nick"),
+                Token::Some,
+                Token::Str("a nickname"),
+                Token::Str("roles"),
+                Token::Seq { len: Some(2) },
+                Token::NewtypeStruct { name: "RoleId" },
+                Token::Str("1"),
+                Token::NewtypeStruct { name: "RoleId" },
+                Token::Str("2"),
+                Token::SeqEnd,
+                Token::StructEnd,
+            ],
+        );
+    }
 }
