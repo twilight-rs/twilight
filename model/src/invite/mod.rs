3ac98c34a496f91be990f1f5d7b779c6ead00ef6
Vivian <vivian@hellyer.dev>
2020-08-18 23:07:54 -0400

    model/invite: add 'inviteguild' type (#404)
    * model/invite: add 'inviteguild' type
    
    Add `invite::InviteGuild` which is used as part of a `invite::Invite`.
    `guild::PartialGuild`s have fields that aren't included in the
    information included with an invite, so an even smaller guild type is
    needed.
    
    Closes #391.
    
    Signed-off-by: Vivian Hellyer <vivian@hellyer.dev>
    
    * cargo fmt
    
    Signed-off-by: Vivian Hellyer <vivian@hellyer.dev>

--- model/src/invite/mod.rs
+++ model/src/invite/mod.rs
@@ -1,9 +1,10 @@
+mod guild;
 mod metadata;
 mod target_user_type;
 
-pub use self::{metadata::InviteMetadata, target_user_type::TargetUserType};
+pub use self::{guild::InviteGuild, metadata::InviteMetadata, target_user_type::TargetUserType};
 
-use crate::{channel::Channel, guild::PartialGuild, user::User};
+use super::{channel::Channel, user::User};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
@@ -12,7 +13,7 @@ pub struct Invite {
     pub approximate_presence_count: Option<u64>,
     pub channel: Channel,
     pub code: String,
-    pub guild: Option<PartialGuild>,
+    pub guild: Option<InviteGuild>,
     pub inviter: Option<User>,
     pub target_user_type: Option<TargetUserType>,
     pub target_user: Option<User>,
