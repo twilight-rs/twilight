3056cd040f17a2734f8892b44a6b2164fac37b95
Vivian Hellyer <vivian@hellyer.dev>
2020-08-21 22:21:34 -0400

    rebase
    Signed-off-by: Vivian Hellyer <vivian@hellyer.dev>

--- model/src/oauth/team/membership_state.rs
+++ model/src/oauth/team/membership_state.rs
@@ -1,8 +1,22 @@
-use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
 
-#[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(
+    Clone, Copy, Debug, Deserialize_repr, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize_repr,
+)]
 #[repr(u8)]
 pub enum TeamMembershipState {
     Invited = 1,
     Accepted = 2,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::TeamMembershipState;
+    use serde_test::Token;
+
+    #[test]
+    fn test_variants() {
+        serde_test::assert_tokens(&TeamMembershipState::Invited, &[Token::U8(1)]);
+        serde_test::assert_tokens(&TeamMembershipState::Accepted, &[Token::U8(2)]);
+    }
 }
