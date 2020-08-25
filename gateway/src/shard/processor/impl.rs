0624335c5b74d7942327fdc9e55ccec0cff6b29b
Vivian Hellyer <vivian@hellyer.dev>
2020-08-24 20:13:53 -0400

    split up reconnect, processing methods
    Signed-off-by: Vivian Hellyer <vivian@hellyer.dev>

--- gateway/src/shard/processor/impl.rs
+++ gateway/src/shard/processor/impl.rs
@@ -317,7 +317,7 @@ impl ShardProcessor {
                     );
 
                     // Inflater gets reset in the reconnect call.
-                    self.reconnect(true).await;
+                    self.reconnect().await;
                     continue;
                 }
                 Err(ReceivingEventError::IntentsDisallowed { shard_id, .. }) => {
@@ -354,7 +354,7 @@ impl ShardProcessor {
             if self.process(&gateway_event).await.is_err() {
                 tracing::debug!("error processing event; reconnecting");
 
-                self.reconnect(true).await;
+                self.reconnect().await;
 
                 continue;
             }
@@ -390,225 +390,151 @@ impl ShardProcessor {
         self.send(identify).await
     }
 
-    #[allow(clippy::too_many_lines)]
     async fn process(&mut self, event: &GatewayEvent) -> Result<(), ProcessError> {
         use GatewayEvent::{
             Dispatch, Heartbeat, HeartbeatAck, Hello, InvalidateSession, Reconnect,
         };
 
         match event {
-            Dispatch(seq, dispatch) => {
-                #[cfg(feature = "metrics")]
-                metrics::counter!("GatewayEvent", 1, "GatewayEvent" => "Dispatch");
-                self.session.set_seq(*seq);
-
-                // this lint is wrong and generates invalid code
-                #[allow(clippy::explicit_deref_methods)]
-                match dispatch.deref() {
-                    DispatchEvent::Ready(ready) => {
-                        self.session.set_stage(Stage::Connected);
-                        self.session.set_id(&ready.session_id);
-
-                        emit::event(
-                            &self.listeners,
-                            Event::ShardConnected(Connected {
-                                heartbeat_interval: self.session.heartbeat_interval(),
-                                shard_id: self.config.shard()[0],
-                            }),
-                        );
-                    }
-                    DispatchEvent::Resumed => {
-                        self.session.set_stage(Stage::Connected);
-                        emit::event(
-                            &self.listeners,
-                            Event::ShardConnected(Connected {
-                                heartbeat_interval: self.session.heartbeat_interval(),
-                                shard_id: self.config.shard()[0],
-                            }),
-                        );
-                        self.session.heartbeats.receive();
-                    }
-                    _ => {}
-                }
-            }
-            Heartbeat(seq) => {
-                #[cfg(feature = "metrics")]
-                metrics::counter!("GatewayEvent", 1, "GatewayEvent" => "Heartbeat");
-                if *seq > self.session.seq() + 1 {
-                    self.resume().await;
-                }
-
-                if let Err(err) = self.session.heartbeat() {
-                    tracing::warn!("error sending heartbeat; reconnecting: {}", err);
-
-                    self.reconnect(true).await;
-                }
-            }
-            Hello(interval) => {
-                #[cfg(feature = "metrics")]
-                metrics::counter!("GatewayEvent", 1, "GatewayEvent" => "Hello");
-                tracing::debug!("got hello with interval {}", interval);
-
-                if self.session.stage() == Stage::Resuming && self.resume.is_some() {
-                    // Safe to unwrap so here as we have just checked that
-                    // it is some.
-                    let (seq, id) = self.resume.take().unwrap();
-                    tracing::warn!("resuming with sequence {}, session id {}", seq, id);
-                    let payload = Resume::new(seq, &id, self.config.token());
-
-                    // Set id so it is correct for next resume.
-                    self.session.set_id(id);
-
-                    if *interval > 0 {
-                        self.session.set_heartbeat_interval(*interval);
-                        self.session.start_heartbeater();
-                    }
-
-                    self.send(payload)
-                        .await
-                        .map_err(|source| ProcessError::SessionSend { source })?;
-                } else {
-                    self.session.set_stage(Stage::Identifying);
-
-                    if *interval > 0 {
-                        self.session.set_heartbeat_interval(*interval);
-                        self.session.start_heartbeater();
-                    }
-
-                    self.identify()
-                        .await
-                        .map_err(|source| ProcessError::SessionSend { source })?;
-                }
-            }
-            HeartbeatAck => {
-                #[cfg(feature = "metrics")]
-                metrics::counter!("GatewayEvent", 1, "GatewayEvent" => "HeartbeatAck");
-                self.session.heartbeats.receive();
-            }
-            InvalidateSession(true) => {
-                #[cfg(feature = "metrics")]
-                metrics::counter!("GatewayEvent", 1, "GatewayEvent" => "InvalidateSessionTrue");
-                tracing::debug!("got request to resume the session");
-                self.resume().await;
-            }
-            InvalidateSession(false) => {
-                #[cfg(feature = "metrics")]
-                metrics::counter!("GatewayEvent", 1, "GatewayEvent" => "InvalidateSessionFalse");
-                tracing::debug!("got request to invalidate the session and reconnect");
-                self.reconnect(true).await;
-            }
-            Reconnect => {
-                #[cfg(feature = "metrics")]
-                metrics::counter!("GatewayEvent", 1, "GatewayEvent" => "Reconnect");
-                tracing::debug!("got request to reconnect");
-                let frame = CloseFrame {
-                    code: CloseCode::Restart,
-                    reason: Cow::Borrowed("Reconnecting"),
-                };
-                self.close(Some(frame))
-                    .await
-                    .map_err(|source| ProcessError::SendingClose { source })?;
-                self.resume().await;
-            }
+            Dispatch(seq, dispatch) => self.process_dispatch(*seq, dispatch.as_ref()),
+            Heartbeat(seq) => self.process_heartbeat(*seq).await,
+            Hello(interval) => self.process_hello(*interval).await?,
+            HeartbeatAck => self.process_heartbeat_ack(),
+            InvalidateSession(resumable) => self.process_invalidate_session(*resumable).await,
+            Reconnect => self.process_reconnect().await?,
         }
 
         Ok(())
     }
 
-    async fn reconnect(&mut self, mut full_reconnect: bool) {
-        tracing::info!("reconnection started");
-        loop {
-            // Await allowance if doing a full reconnect
-            if full_reconnect {
-                let shard = self.config.shard();
-                self.config.queue.request(shard).await;
-            }
+    fn process_dispatch(&self, seq: u64, dispatch: &DispatchEvent) {
+        #[cfg(feature = "metrics")]
+        metrics::counter!("GatewayEvent", 1, "GatewayEvent" => "Dispatch");
+        self.session.set_seq(seq);
 
-            if full_reconnect {
+        // this lint is wrong and generates invalid code
+        #[allow(clippy::explicit_deref_methods)]
+        match dispatch.deref() {
+            DispatchEvent::Ready(ready) => {
+                self.session.set_stage(Stage::Connected);
+                self.session.set_id(&ready.session_id);
+
                 emit::event(
                     &self.listeners,
-                    Event::ShardReconnecting(Reconnecting {
-                        shard_id: self.config.shard()[0],
-                    }),
-                );
-            } else {
-                emit::event(
-                    &self.listeners,
-                    Event::ShardResuming(Resuming {
-                        seq: self.session.seq(),
+                    Event::ShardConnected(Connected {
+                        heartbeat_interval: self.session.heartbeat_interval(),
                         shard_id: self.config.shard()[0],
                     }),
                 );
             }
-
-            let new_stream = match Self::connect(&self.url).await {
-                Ok(s) => s,
-                Err(why) => {
-                    tracing::warn!("reconnecting failed: {:?}", why);
-                    full_reconnect = true;
-                    continue;
-                }
-            };
-
-            let (new_forwarder, new_rx, new_tx) = SocketForwarder::new(new_stream);
-            tokio::spawn(async move {
-                new_forwarder.run().await;
-            });
-
-            self.rx = new_rx;
-            self.session = Arc::new(Session::new(new_tx));
-            match self.wtx.broadcast(Arc::clone(&self.session)) {
-                Ok(_) => (),
-                Err(why) => {
-                    tracing::warn!(
-                        "broadcast of new session failed, \
-                         this should not happen, please open \
-                         an issue on the twilight repo: {}",
-                        why
-                    );
-                    tracing::warn!(
-                        "after this many of the commands on the \
-                         shard will no longer work"
-                    );
-                }
-            };
-
-            if !full_reconnect {
-                self.session.set_stage(Stage::Resuming);
+            DispatchEvent::Resumed => {
+                self.session.set_stage(Stage::Connected);
+                emit::event(
+                    &self.listeners,
+                    Event::ShardConnected(Connected {
+                        heartbeat_interval: self.session.heartbeat_interval(),
+                        shard_id: self.config.shard()[0],
+                    }),
+                );
+                self.session.heartbeats.receive();
             }
-
-            self.inflater.reset();
-
-            break;
+            _ => {}
         }
-
-        emit::event(
-            &self.listeners,
-            Event::ShardConnecting(Connecting {
-                gateway: self.url.clone(),
-                shard_id: self.config.shard()[0],
-            }),
-        );
     }
 
-    async fn resume(&mut self) {
-        tracing::info!("resuming shard {:?}", self.config.shard());
-        self.session.set_stage(Stage::Resuming);
-        self.session.stop_heartbeater();
+    fn process_heartbeat_ack(&self) {
+        #[cfg(feature = "metrics")]
+        metrics::counter!("GatewayEvent", 1, "GatewayEvent" => "HeartbeatAck");
 
-        let seq = self.session.seq();
+        self.session.heartbeats.receive();
+    }
 
-        let id = if let Some(id) = self.session.id() {
-            id
+    async fn process_heartbeat(&mut self, seq: u64) {
+        #[cfg(feature = "metrics")]
+        metrics::counter!("GatewayEvent", 1, "GatewayEvent" => "Heartbeat");
+
+        if seq > self.session.seq() + 1 {
+            self.resume().await;
+        }
+
+        if let Err(err) = self.session.heartbeat() {
+            tracing::warn!("error sending heartbeat; reconnecting: {}", err);
+
+            self.reconnect().await;
+        }
+    }
+
+    async fn process_hello(&mut self, interval: u64) -> Result<(), ProcessError> {
+        #[cfg(feature = "metrics")]
+        metrics::counter!("GatewayEvent", 1, "GatewayEvent" => "Hello");
+
+        tracing::debug!("got hello with interval {}", interval);
+
+        if self.session.stage() == Stage::Resuming && self.resume.is_some() {
+            // Safe to unwrap so here as we have just checked that
+            // it is some.
+            let (seq, id) = self.resume.take().unwrap();
+            tracing::warn!("resuming with sequence {}, session id {}", seq, id);
+            let payload = Resume::new(seq, &id, self.config.token());
+
+            // Set id so it is correct for next resume.
+            self.session.set_id(id);
+
+            if interval > 0 {
+                self.session.set_heartbeat_interval(interval);
+                self.session.start_heartbeater();
+            }
+
+            self.send(payload)
+                .await
+                .map_err(|source| ProcessError::SessionSend { source })?;
         } else {
-            tracing::warn!("session id unavailable, reconnecting");
-            self.reconnect(true).await;
-            return;
+            self.session.set_stage(Stage::Identifying);
+
+            if interval > 0 {
+                self.session.set_heartbeat_interval(interval);
+                self.session.start_heartbeater();
+            }
+
+            self.identify()
+                .await
+                .map_err(|source| ProcessError::SessionSend { source })?;
+        }
+
+        Ok(())
+    }
+
+    async fn process_invalidate_session(&mut self, resumable: bool) {
+        if resumable {
+            #[cfg(feature = "metrics")]
+            metrics::counter!("GatewayEvent", 1, "GatewayEvent" => "InvalidateSessionTrue");
+
+            tracing::debug!("got request to resume the session");
+            self.resume().await;
+        } else {
+            #[cfg(feature = "metrics")]
+            metrics::counter!("GatewayEvent", 1, "GatewayEvent" => "InvalidateSessionFalse");
+
+            tracing::debug!("got request to invalidate the session and reconnect");
+            self.reconnect().await;
+        }
+    }
+
+    async fn process_reconnect(&mut self) -> Result<(), ProcessError> {
+        #[cfg(feature = "metrics")]
+        metrics::counter!("GatewayEvent", 1, "GatewayEvent" => "Reconnect");
+
+        tracing::debug!("got request to reconnect");
+        let frame = CloseFrame {
+            code: CloseCode::Restart,
+            reason: Cow::Borrowed("Reconnecting"),
         };
+        self.session
+            .close(Some(frame))
+            .map_err(|source| ProcessError::SendingClose { source })?;
+        self.resume().await;
 
-        self.resume = Some((seq, id));
-
-        self.reconnect(false).await;
+        Ok(())
     }
 
     pub async fn send(&mut self, payload: impl Serialize) -> Result<(), SessionSendError> {
@@ -618,7 +544,7 @@ impl ShardProcessor {
                 tracing::warn!("sending message failed: {:?}", source);
                 tracing::info!("reconnecting shard {:?}", self.config.shard());
 
-                self.reconnect(true).await;
+                self.reconnect().await;
 
                 Err(SessionSendError::Sending { source })
             }
@@ -628,13 +554,6 @@ impl ShardProcessor {
                 Err(SessionSendError::Serializing { source })
             }
         }
-    }
-
-    async fn close(
-        &mut self,
-        close_frame: Option<CloseFrame<'static>>,
-    ) -> Result<(), TrySendError<Message>> {
-        self.session.close(close_frame)
     }
 
     /// # Errors
@@ -745,6 +664,113 @@ impl ShardProcessor {
         tracing::debug!("Shook hands with remote");
 
         Ok(stream)
+    }
+
+    /// Perform a full reconnect to the gateway, instantiating a new session.
+    async fn reconnect(&mut self) {
+        tracing::info!("reconnection started");
+
+        loop {
+            // Await allowance when doing a full reconnect.
+            self.config.queue.request(self.config.shard()).await;
+
+            emit::event(
+                &self.listeners,
+                Event::ShardReconnecting(Reconnecting {
+                    shard_id: self.config.shard()[0],
+                }),
+            );
+
+            let stream = match Self::connect(&self.url).await {
+                Ok(s) => s,
+                Err(why) => {
+                    tracing::warn!("reconnecting failed: {:?}", why);
+
+                    continue;
+                }
+            };
+
+            self.set_session(stream, Stage::Connected);
+
+            break;
+        }
+
+        emit::event(
+            &self.listeners,
+            Event::ShardConnecting(Connecting {
+                gateway: self.url.clone(),
+                shard_id: self.config.shard()[0],
+            }),
+        );
+    }
+
+    /// Resume a session if possible, defaulting to instantiating a new
+    /// connection.
+    async fn resume(&mut self) {
+        tracing::info!("resuming shard {:?}", self.config.shard());
+        self.session.set_stage(Stage::Resuming);
+        self.session.stop_heartbeater();
+
+        let seq = self.session.seq();
+
+        let id = if let Some(id) = self.session.id() {
+            id
+        } else {
+            tracing::warn!("session id unavailable, reconnecting");
+            self.reconnect().await;
+            return;
+        };
+
+        self.resume = Some((seq, id));
+
+        if let Err(why) = self.try_resume().await {
+            tracing::warn!(
+                seq = seq,
+                session_id = ?self.session.id(),
+                shard_id = self.config.shard()[0],
+                "failed to resume session: {:?}",
+                why,
+            );
+
+            self.reconnect().await;
+        }
+    }
+
+    /// Attempt to resume a session.
+    async fn try_resume(&mut self) -> Result<(), ConnectingError> {
+        emit::event(
+            &self.listeners,
+            Event::ShardResuming(Resuming {
+                seq: self.session.seq(),
+                shard_id: self.config.shard()[0],
+            }),
+        );
+
+        let stream = Self::connect(&self.url).await?;
+
+        self.set_session(stream, Stage::Resuming);
+
+        Ok(())
+    }
+
+    /// Set the session with a new connection.
+    ///
+    /// Set the session details and create and run a new socket forwarder for a
+    /// new websocket connection.
+    fn set_session(&mut self, stream: ShardStream, stage: Stage) {
+        let (forwarder, rx, tx) = SocketForwarder::new(stream);
+
+        tokio::spawn(forwarder.run());
+
+        self.rx = rx;
+        self.session = Arc::new(Session::new(tx));
+
+        if let Err(why) = self.wtx.broadcast(Arc::clone(&self.session)) {
+            tracing::error!("failed to broadcast new session: {:?}", why);
+        }
+
+        self.session.set_stage(stage);
+        self.inflater.reset();
     }
 
     /// Parse a gateway event from a string using `serde_json`.
