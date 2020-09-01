71396cd260bbcbaf17c355b0948422d02e3b0da4
Valdemar Erk <valdemar@erk.io>
2020-09-01 18:46:56 +0200

    Added more benchmarks and made the benchmark be more eqivalant
    Signed-off-by: Valdemar Erk <valdemar@erk.io>

--- command-parser/benches/prefix.rs
+++ command-parser/benches/prefix.rs
@@ -1,24 +1,29 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::{borrow::Cow, collections::{BTreeSet, HashSet}};
+use patricia_tree::PatriciaSet;
 
 fn btreeset(set: &BTreeSet<Cow<'static, str>>) {
-    set.get("!").unwrap();
+    set.iter().find(|item| ("!command").starts_with(item.as_ref())).unwrap();
 }
 
 fn hashset(set: &HashSet<Cow<'static, str>>) {
-    set.get("!").unwrap();
+    set.iter().find(|item| ("!command").starts_with(item.as_ref())).unwrap();
 }
 
 fn vec(items: &[Cow<'static, str>]) {
-    items.iter().find(|item| item.as_ref() == "!").unwrap();
+    items.iter().find(|item| ("!command").starts_with(item.as_ref())).unwrap();
 }
 
 fn vec_worst_case(items: &[Cow<'static, str>]) {
-    items.iter().find(|item| item.as_ref() == "botname").unwrap();
+    items.iter().find(|item| ("!command").starts_with(item.as_ref())).unwrap();
+}
+
+fn patricia_tree(items: &PatriciaSet) {
+    let _ = items.get_longest_common_prefix("!command").is_some();
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("btreeset", |b| {
+    c.bench_function("p: btreeset", |b| {
         let mut set = BTreeSet::new();
         set.insert(Cow::from("!"));
         set.insert(Cow::from("botname"));
@@ -27,7 +32,7 @@ fn criterion_benchmark(c: &mut Criterion) {
             btreeset(&set);
         })
     });
-    c.bench_function("hashset", |b| {
+    c.bench_function("p: hashset", |b| {
         let mut set = HashSet::new();
         set.insert(Cow::from("!"));
         set.insert(Cow::from("botname"));
@@ -36,7 +41,7 @@ fn criterion_benchmark(c: &mut Criterion) {
             hashset(&set);
         })
     });
-    c.bench_function("vec", |b| {
+    c.bench_function("p: vec", |b| {
         let items = vec![Cow::from("!"), Cow::from("botname")];
 
         b.iter(|| {
@@ -44,11 +49,21 @@ fn criterion_benchmark(c: &mut Criterion) {
         });
     });
 
-    c.bench_function("vec_worst_case", |b| {
+    c.bench_function("p: vec_worst_case", |b| {
         let items = vec![Cow::from("!"), Cow::from("botname")];
 
         b.iter(|| {
             vec_worst_case(&items);
+        });
+    });
+
+    c.bench_function("p: patricia_tree", |b| {
+        let mut set = PatriciaSet::new();
+        set.insert("!");
+        set.insert("botname");
+
+        b.iter(|| {
+            patricia_tree(&set);
         });
     });
 }
