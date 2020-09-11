0e99f76a57cea1208c0ba492925b150d41365beb
Valdemar Erk <valdemar@erk.io>
2020-09-11 16:17:48 +0200

    Feedback

--- http/src/request/channel/delete_channel_permission.rs
+++ http/src/request/channel/delete_channel_permission.rs
@@ -4,7 +4,7 @@ use twilight_model::id::{ChannelId, RoleId, UserId};
 
 /// Clear the permissions for a target ID in a channel.
 ///
-/// The target ID has to be set with one of the associated methods.
+/// The target ID must be set with one of the associated methods.
 pub struct DeleteChannelPermission<'a> {
     channel_id: ChannelId,
     http: &'a Client,
@@ -15,12 +15,12 @@ impl<'a> DeleteChannelPermission<'a> {
         Self { channel_id, http }
     }
 
-    /// Delete a override for a member.
+    /// Delete a override for an member.
     pub fn member(self, user_id: impl Into<UserId>) -> DeleteChannelPermissionConfigured<'a> {
         self.configure(user_id.into().0)
     }
 
-    /// Delete a override for a role.
+    /// Delete a override for an role.
     pub fn role(self, role_id: impl Into<RoleId>) -> DeleteChannelPermissionConfigured<'a> {
         self.configure(role_id.into().0)
     }
