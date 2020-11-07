89c4eb7dc9e6a0308024bce6e972ccb28ba98c7e
CHamburr <hi@chamburr.xyz>
2020-11-07 11:17:15 +0800

    model: add missing gateway codes

--- model/src/gateway/mod.rs
+++ model/src/gateway/mod.rs
@@ -1,11 +1,13 @@
-pub mod close_code;
 pub mod connection_info;
 pub mod event;
 pub mod payload;
 pub mod presence;
 
+mod close_code;
 mod intents;
 mod opcode;
 mod session_start_limit;
 
-pub use self::{intents::Intents, opcode::OpCode, session_start_limit::SessionStartLimit};
+pub use self::{
+    close_code::CloseCode, intents::Intents, opcode::OpCode, session_start_limit::SessionStartLimit,
+};
