77f191ab49a738386f2d93232d39d614f0ff0f96
Sam Kirby <sam.kirby94@hotmail.co.uk>
2020-11-15 01:39:03 +0000

    model: fix invites
    Discord returns only a partial channel, which cannot be deserialized into a full channel.
    
    This commit adds a model for the partial channel (named `InviteChannel` following the convention set by `InviteGuild`)
    
    Associated tests updated, and test for `InviteChannel` added.

---
+++ model/src/invite/channel.rs
@@ -0,0 +1,48 @@
+use crate::channel::ChannelType;
+use crate::id::ChannelId;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
+pub struct InviteChannel {
+    /// ID of the channel
+    pub id: ChannelId,
+    /// Name of the channel
+    pub name: Option<String>,
+    /// Type of the channel
+    #[serde(rename = "type")]
+    pub kind: ChannelType,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{ChannelId, ChannelType, InviteChannel};
+    use serde_test::Token;
+
+    #[test]
+    fn test_invite_channel() {
+        let value = InviteChannel {
+            id: ChannelId(1),
+            name: Some("channel name".to_owned()),
+            kind: ChannelType::GuildText,
+        };
+
+        serde_test::assert_tokens(
+            &value,
+            &[
+                Token::Struct {
+                    name: "InviteChannel",
+                    len: 3,
+                },
+                Token::Str("id"),
+                Token::NewtypeStruct { name: "ChannelId" },
+                Token::Str("1"),
+                Token::Str("name"),
+                Token::Some,
+                Token::Str("channel name"),
+                Token::Str("type"),
+                Token::U8(0),
+                Token::StructEnd,
+            ],
+        )
+    }
+}
