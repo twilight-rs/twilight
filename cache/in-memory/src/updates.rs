86e5607cff38af90274cd3bb4490bc89137d1f00
Vivian Hellyer <vivian@hellyer.dev>
2020-11-24 21:04:41 -0500

    feat(model): support message mention members (#609)
    Support partial members included in mentions within messages by creating
    a new `Mention` type.
    
    Closes #566.
    
    Approved-by: Cassandra McCarthy <cassie@7596ff.com>
    Approved-by: Valdemar Erk <valdemar@erk.io>
    Merged-by: Vivian Hellyer <vivian@hellyer.dev>
    Signed-off-by: Vivian Hellyer <vivian@hellyer.dev>

--- cache/in-memory/src/updates.rs
+++ cache/in-memory/src/updates.rs
@@ -778,6 +778,7 @@ mod tests {
                 joined_at: None,
                 mute: false,
                 nick: Some("member nick".to_owned()),
+                premium_since: None,
                 roles: Vec::new(),
             }),
             mention_channels: Vec::new(),
