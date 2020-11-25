043cb9003704e6cb7e79a15f67c9d09bc591f6f6
doge <shadowphoenixcoc@gmail.com>
2020-11-24 21:45:13 -0500

    feat (json serialization options for Information struct)

--- gateway/src/shard/impl.rs
+++ gateway/src/shard/impl.rs
@@ -147,7 +147,7 @@ impl From<ConnectingError> for ShardStartError {
 
 /// Information about a shard, including its latency, current session sequence,
 /// and connection stage.
-#[derive(Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Information {
     id: u64,
     latency: Latency,
