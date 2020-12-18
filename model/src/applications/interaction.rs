d149af259a266d42f52c90cc57f3994de2dd8922
Valdemar Erk <valdemar@erk.io>
2020-12-18 18:23:59 +0100

    Add test
    Signed-off-by: Valdemar Erk <valdemar@erk.io>

--- model/src/applications/interaction.rs
+++ model/src/applications/interaction.rs
@@ -142,3 +142,96 @@ impl InteractionEnvelope {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::applications::*;
+    use crate::user::User;
+    use crate::guild::Permissions;
+    use crate::user::UserFlags;
+    use crate::guild::PartialMember;
+    use crate::applications::interaction_data::ApplicationCommandInteractionData;
+    use crate::id::*;
+
+    #[test]
+    fn test_interaction() {
+        let json = r#"{
+    "type": 2,
+    "token": "A_UNIQUE_TOKEN",
+    "member": {
+        "user": {
+            "id": "53908232506183680",
+            "username": "Mason",
+            "avatar": "a_d5efa99b3eeaa7dd43acca82f5692432",
+            "discriminator": "1337",
+            "public_flags": 131141
+        },
+        "roles": ["539082325061836999"],
+        "premium_since": null,
+        "permissions": "2147483647",
+        "pending": false,
+        "nick": null,
+        "mute": false,
+        "joined_at": "2017-03-13T19:19:14.040000+00:00",
+        "is_pending": false,
+        "deaf": false
+    },
+    "id": "786008729715212338",
+    "guild_id": "290926798626357999",
+    "data": {
+        "options": [{
+            "name": "cardname",
+            "value": "The Gitrog Monster"
+        }],
+        "name": "cardsearch",
+        "id": "771825006014889984"
+    },
+    "channel_id": "645027906669510667"
+}"#;
+
+        let expected = Interaction::WithGuildId(GuildInteraction {
+            data: InteractionData::ApplicationCommand(ApplicationCommandInteractionData {
+                options: vec![InteractionDataOption::String {
+                    name: "cardname".to_string(),
+                    value: "The Gitrog Monster".to_string(),
+                }],
+                name: "cardsearch".to_string(),
+                id: 771825006014889984.into(),
+            }),
+            guild_id: 290926798626357999.into(),
+            channel_id: 645027906669510667.into(),
+            member: PartialMember {
+                user: Some(User {
+                    id: UserId(53908232506183680),
+                    name: "Mason".to_string(),
+                    avatar: Some("a_d5efa99b3eeaa7dd43acca82f5692432".to_string()),
+                    discriminator: 1337.to_string(),
+                    public_flags: UserFlags::from_bits(131141),
+                    bot: false,
+                    email: None,
+                    flags: None,
+                    locale: None,
+                    mfa_enabled: None,
+                    premium_type: None,
+                    system: None,
+                    verified: None,
+                }),
+                roles: vec![539082325061836999.into()],
+                permissions: Permissions::from_bits(2147483647),
+                nick: None,
+                mute: false,
+                joined_at: Some("2017-03-13T19:19:14.040000+00:00".to_string()),
+                deaf: false
+            },
+            interaction: BaseInteraction {
+                id: 786008729715212338.into(),
+                kind: InteractionType::ApplicationCommand,
+                token: "A_UNIQUE_TOKEN".to_string(),
+            }
+        });
+
+        let actual = serde_json::from_str::<Interaction>(&json).unwrap();
+
+        assert_eq!(expected, actual);
+    }
+}
