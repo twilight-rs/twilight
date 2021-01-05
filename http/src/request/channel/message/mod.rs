e4dc7c8fc9d748fdd8eafd7633807fbc2ed679b8
Valdemar Erk <valdemar@erk.io>
2021-01-05 22:29:53 +0100

    Remove the re-export of allowed_mentions in channel/message/mod.rs
    as per comment:
    ```rust
    // 0.3+: Remove this re-export.
    //
    // This re-export is here because `allowed_mentions` was moved up a module to
    // `request::channel`: <https://github.com/twilight-rs/twilight/pull/643>
    ```
    
    Signed-off-by: Valdemar Erk <valdemar@erk.io>

--- http/src/request/channel/message/mod.rs
+++ http/src/request/channel/message/mod.rs
@@ -16,9 +16,3 @@ pub use self::{
     update_message::UpdateMessage,
 };
 pub use super::super::validate::EmbedValidationError;
-
-// 0.3+: Remove this re-export.
-//
-// This re-export is here because `allowed_mentions` was moved up a module to
-// `request::channel`: <https://github.com/twilight-rs/twilight/pull/643>
-pub use super::allowed_mentions;
