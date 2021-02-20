0aadd659fb80fa168fb69160a7038c76f003c85d
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-02-20 18:08:31 +0800

    Update get_channel_messages_configured.rs

--- http/src/request/channel/message/get_channel_messages_configured.rs
+++ http/src/request/channel/message/get_channel_messages_configured.rs
@@ -54,7 +54,7 @@ impl Error for GetChannelMessagesConfiguredError {}
 
 /// Type of [`GetChannelMessagesConfiguredError`] that occurred.
 #[derive(Debug)]
-#[non_exhaustive]
+#[cfg_attr(feature = "non_exhaustive", non_exhaustive)]
 pub enum GetChannelMessagesConfiguredErrorType {
     /// The maximum number of messages to retrieve is either 0 or more than 100.
     LimitInvalid {
