51cf7371d0d1f6610fd70143159add1bd97ce8b7
Cassandra McCarthy <cassie@7596ff.com>
2021-03-24 12:39:55 -0400

    feat(http): add guild templates
    https://discord.com/developers/docs/resources/template
    
    Templates use a custom guild type `TemplateGuild`, which itself uses a
    custom role type `TemplateRole`.  The deserializer for
    `PermissionOverwrite` has been updated to support both integers and
    strings.
    
    Signed-off-by: Cassandra McCarthy <cassie@7596ff.com>

--- model/src/channel/permission_overwrite.rs
+++ model/src/channel/permission_overwrite.rs
@@ -2,12 +2,38 @@ use crate::{
     guild::Permissions,
     id::{RoleId, UserId},
 };
-use serde::{
-    de::{Deserializer, Error as DeError},
-    ser::SerializeStruct,
-    Deserialize, Serialize, Serializer,
-};
+use serde::{de::Deserializer, ser::SerializeStruct, Deserialize, Serialize, Serializer};
 use serde_repr::{Deserialize_repr, Serialize_repr};
+
+pub(crate) mod integer {
+    use serde::de::{Deserializer, Error as DeError, Visitor};
+    use std::{
+        fmt::{Formatter, Result as FmtResult},
+        marker::PhantomData,
+    };
+
+    struct IdVisitor(PhantomData<u64>);
+
+    impl<'de> Visitor<'de> for IdVisitor {
+        type Value = u64;
+
+        fn expecting(&self, f: &mut Formatter<'_>) -> FmtResult {
+            f.write_str("string or integer snowflake")
+        }
+
+        fn visit_u64<E: DeError>(self, value: u64) -> Result<Self::Value, E> {
+            Ok(value)
+        }
+
+        fn visit_str<E: DeError>(self, value: &str) -> Result<Self::Value, E> {
+            value.parse().map_err(DeError::custom)
+        }
+    }
+
+    pub fn deserialize<'de, D: Deserializer<'de>>(deserializer: D) -> Result<u64, D::Error> {
+        deserializer.deserialize_any(IdVisitor(PhantomData))
+    }
+}
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct PermissionOverwrite {
@@ -26,7 +52,8 @@ pub enum PermissionOverwriteType {
 struct PermissionOverwriteData {
     allow: Permissions,
     deny: Permissions,
-    id: String,
+    #[serde(deserialize_with = "integer::deserialize")]
+    id: u64,
     #[serde(rename = "type")]
     kind: PermissionOverwriteTargetType,
 }
@@ -51,13 +78,13 @@ impl<'de> Deserialize<'de> for PermissionOverwrite {
 
         let kind = match data.kind {
             PermissionOverwriteTargetType::Member => {
-                let id = UserId(data.id.parse().map_err(DeError::custom)?);
+                let id = UserId(data.id);
                 tracing::trace!(id = %id.0, kind = ?data.kind);
 
                 PermissionOverwriteType::Member(id)
             }
             PermissionOverwriteTargetType::Role => {
-                let id = RoleId(data.id.parse().map_err(DeError::custom)?);
+                let id = RoleId(data.id);
                 tracing::trace!(id = %id.0, kind = ?data.kind);
 
                 PermissionOverwriteType::Role(id)
@@ -125,6 +152,39 @@ mod tests {
             overwrite
         );
         assert_eq!(serde_json::to_string_pretty(&overwrite).unwrap(), input);
+    }
+
+    #[test]
+    fn test_blank_overwrite() {
+        // Test integer deser used in guild templates.
+        let blank_overwrite = PermissionOverwrite {
+            allow: Permissions::CREATE_INVITE,
+            deny: Permissions::KICK_MEMBERS,
+            kind: PermissionOverwriteType::Member(UserId(0)),
+        };
+
+        let input = r#"{
+  "allow": "1",
+  "deny": "2",
+  "id": 0,
+  "type": 1
+}"#;
+
+        let output = r#"{
+  "allow": "1",
+  "deny": "2",
+  "id": "0",
+  "type": 1
+}"#;
+
+        assert_eq!(
+            serde_json::from_str::<PermissionOverwrite>(input).unwrap(),
+            blank_overwrite
+        );
+        assert_eq!(
+            serde_json::to_string_pretty(&blank_overwrite).unwrap(),
+            output
+        );
     }
 
     #[test]
