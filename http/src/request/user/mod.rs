1d26cbb0907d0a95c085ea5d9323eb4f73187072
Vivian Hellyer <vivian@hellyer.dev>
2021-04-17 14:44:16 -0400

    chore(http): remove get current user dms request
    Remove the request for retrieving the current user's DMs.
    
    This endpoint has been removed from the documentation, per
    <https://github.com/discord/discord-api-docs/commit/ca5064c46dc18e13cd2d65dea735eb44a52998de>.
    
    Signed-off-by: Vivian Hellyer <vivian@hellyer.dev>

--- http/src/request/user/mod.rs
+++ http/src/request/user/mod.rs
@@ -4,14 +4,12 @@ pub mod update_current_user;
 mod create_private_channel;
 mod get_current_user;
 mod get_current_user_connections;
-mod get_current_user_private_channels;
 mod get_user;
 mod leave_guild;
 
 pub use self::{
     create_private_channel::CreatePrivateChannel, get_current_user::GetCurrentUser,
     get_current_user_connections::GetCurrentUserConnections,
-    get_current_user_guilds::GetCurrentUserGuilds,
-    get_current_user_private_channels::GetCurrentUserPrivateChannels, get_user::GetUser,
-    leave_guild::LeaveGuild, update_current_user::UpdateCurrentUser,
+    get_current_user_guilds::GetCurrentUserGuilds, get_user::GetUser, leave_guild::LeaveGuild,
+    update_current_user::UpdateCurrentUser,
 };
