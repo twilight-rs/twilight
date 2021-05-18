7f1f351b85cda7b8ec5e23fae22252f0a38db2f7
Vivian Hellyer <vivian@hellyer.dev>
2021-05-18 19:32:33 -0400

    Update thread_members_update.rs

--- model/src/gateway/payload/thread_members_update.rs
+++ model/src/gateway/payload/thread_members_update.rs
@@ -6,12 +6,12 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub struct ThreadMembersUpdate {
-    pub id: ChannelId,
-    pub guild_id: GuildId,
-    /// Max value of 50.
-    pub member_count: u8,
     #[serde(default)]
     pub added_members: Vec<ThreadMember>,
+    pub guild_id: GuildId,
+    pub id: ChannelId,
+    /// Max value of 50.
+    pub member_count: u8,
     #[serde(default)]
     pub removed_member_ids: Vec<UserId>,
 }
