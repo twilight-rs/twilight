c9c07248023dd1253c463c046396b9a4210988b9
Vivian Hellyer <vivian@hellyer.dev>
2021-05-19 15:52:38 -0400

    docs: update readme to mention 0.4 (#815)
    Update the readme to mention 0.4 instead of 0.3.

--- twilight/src/lib.rs
+++ twilight/src/lib.rs
@@ -22,10 +22,10 @@
 //! `Cargo.toml`'s `[dependencies]` section:
 //!
 //! ```toml
-//! twilight-cache-inmemory = "0.3"
-//! twilight-gateway = "0.3"
-//! twilight-http = "0.3"
-//! twilight-model = "0.3"
+//! twilight-cache-inmemory = "0.4"
+//! twilight-gateway = "0.4"
+//! twilight-http = "0.4"
+//! twilight-model = "0.4"
 //! ```
 //!
 //! If you need any other functionality that Twilight provides, you can just add
