b0e1e3ee7e14becb9178209fdee7ddb526fcd5d5
Cassandra McCarthy <cassie@7596ff.com>
2021-05-21 19:37:17 -0400

    feat(http, model): implement stage instances (#812)
    Adds:
    * `http` `Client::create_stage_instance`
    * `http` `Client::delete_stage_instance`
    * `http` `Client::stage_instance`
    * `http` `Client::update_stage_instance`
    * `model` `StageId`
    * `model` `StageInstance`
    
    https://github.com/discord/discord-api-docs/commit/f019fc358047050513c623f3639b6e96809f9280

---
+++ http/src/request/channel/stage/mod.rs
@@ -0,0 +1,10 @@
+pub mod create_stage_instance;
+pub mod update_stage_instance;
+
+mod delete_stage_instance;
+mod get_stage_instance;
+
+pub use self::{
+    create_stage_instance::CreateStageInstance, delete_stage_instance::DeleteStageInstance,
+    get_stage_instance::GetStageInstance, update_stage_instance::UpdateStageInstance,
+};
