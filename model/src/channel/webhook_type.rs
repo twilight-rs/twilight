822992cac61e60c730d39b94023b89d8a9fe3240
Cassandra McCarthy <cassie@7596ff.com>
2021-05-31 12:35:25 -0400

    feat(model/webhook_type): add 'application' variant (#842)
    Add the `Application` variant to `WebhookType`.
    
    Documentation:
    <https://discord.com/developers/docs/resources/webhook#webhook-object-webhook-types>

--- model/src/channel/webhook_type.rs
+++ model/src/channel/webhook_type.rs
@@ -7,6 +7,8 @@ use serde_repr::{Deserialize_repr, Serialize_repr};
 pub enum WebhookType {
     Incoming = 1,
     ChannelFollower = 2,
+    /// Webhooks used with interactions.
+    Application = 3,
 }
 
 impl Default for WebhookType {
@@ -29,5 +31,6 @@ mod tests {
     fn test_variants() {
         serde_test::assert_tokens(&WebhookType::Incoming, &[Token::U8(1)]);
         serde_test::assert_tokens(&WebhookType::ChannelFollower, &[Token::U8(2)]);
+        serde_test::assert_tokens(&WebhookType::Application, &[Token::U8(3)]);
     }
 }
