02fcac5024c53c581078d8a0383b95da334f244f
BlackHoleFox <blackholefoxdev@gmail.com>
2021-06-05 17:54:55 -0500

    chore(model, cache): Deprecate guild region field (#887)
    Deprecates the region field on guild. 
    
    Fixes #884.

--- model/src/guild/partial_guild.rs
+++ model/src/guild/partial_guild.rs
@@ -42,6 +42,7 @@ pub struct PartialGuild {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub premium_subscription_count: Option<u64>,
     pub premium_tier: PremiumTier,
+    #[deprecated(since = "0.4.3", note = "no longer provided by discord, see #884")]
     pub region: String,
     pub roles: Vec<Role>,
     pub rules_channel_id: Option<ChannelId>,
@@ -69,6 +70,7 @@ mod tests {
     #[allow(deprecated)]
     #[test]
     fn test_partial_guild() {
+        #[allow(deprecated)]
         let value = PartialGuild {
             id: GuildId(1),
             afk_channel_id: Some(ChannelId(2)),
