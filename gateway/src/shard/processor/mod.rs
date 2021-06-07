fa9a5a76633ed2da0f87b3e0d62ad355868ea06f
Vivian Hellyer <vivian@hellyer.dev>
2021-06-07 16:18:39 -0400

    Simplify gateway processor compression usage (#896)
    refactor(gateway/processor): simplify compression
    
    Simplify the internal compression interface by moving compression
    feature toggles from the shard processor to a new `compression` module.
    The compression module includes the Inflater and a new `Compression`
    struct exposing unified methods that internally toggle based on whether
    compression is enabled.
    
    For example, code like this:
    
    ```rust
    let buf_ref = self.inflater.buffer_mut();
    let buf_ref = self.buffer.as_mut_slice();
    ```
    
    is now like this:
    
    ```rust
    let buffer = self.compression.buffer_slice_mut();
    ```
    
    and `tracing` emits - which had to be duplicated other than a
    call - have been rewritten from:
    
    ```rust
    tracing::warn!(
        json = ?self.inflater.buffer_ref(),
        shard_id = self.config.shard()[0],
        shard_total = self.config.shard()[1],
        seq = self.session.seq(),
        stage = ?self.session.stage(),
        "received payload without opcode",
    );
    tracing::warn!(
        json = ?self.buffer,
        shard_id = self.config.shard()[0],
        shard_total = self.config.shard()[1],
        seq = self.session.seq(),
        stage = ?self.session.stage(),
        "received payload without opcode",
    );
    ```
    
    to:
    
    ```rust
    tracing::warn!(
        json = ?self.compression.buffer_slice_ref(),
        shard_id = self.config.shard()[0],
        shard_total = self.config.shard()[1],
        seq = self.session.seq(),
        stage = ?self.session.stage(),
        "received payload without opcode",
    );
    ```

--- gateway/src/shard/processor/mod.rs
+++ gateway/src/shard/processor/mod.rs
@@ -1,12 +1,10 @@
 pub mod heartbeat;
 
+mod compression;
 mod r#impl;
 mod session;
 mod socket_forwarder;
 mod throttle;
-
-#[cfg(feature = "compression")]
-mod inflater;
 
 pub use self::{
     heartbeat::Latency,
