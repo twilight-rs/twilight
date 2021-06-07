34c3f2ceb9b2596a1aaf345d6125dbf21b0a252a
Cassandra McCarthy <cassie@7596ff.com>
2021-06-07 15:57:01 -0400

    chore!(cache,model): remove deprecated nsfw field (#890)
    BREAKING CHANGE: This PR removes the `nsfw` field on guild models, which
    has been replaced by Discord with `NSFWLevel`.

--- cache/in-memory/src/updates.rs
+++ cache/in-memory/src/updates.rs
@@ -1022,7 +1022,6 @@ mod tests {
         cache
     }
 
-    #[allow(deprecated)]
     #[test]
     fn test_guild_update() {
         let cache = InMemoryCache::new();
@@ -1052,7 +1051,6 @@ mod tests {
             members: Vec::new(),
             mfa_level: MfaLevel::None,
             name: "test".to_owned(),
-            nsfw: false,
             nsfw_level: NSFWLevel::Default,
             owner_id: UserId(1),
             owner: None,
@@ -1097,7 +1095,6 @@ mod tests {
             member_count: guild.member_count,
             mfa_level: guild.mfa_level,
             name: "test2222".to_owned(),
-            nsfw: false,
             nsfw_level: guild.nsfw_level,
             owner_id: UserId(2),
             owner: guild.owner,
