e4ab7e0e47871d43a004434ec78fe5992a575801
Cassandra McCarthy <cassie@7596ff.com>
2021-07-01 10:15:24 -0400

    feat(model): `Webhook::{source_channel, source_guild}` (#961)
    Moves `Webhook` and `WebhookType` to a public module, and keeps
    re-exports.
    
    Fixes #841.

---
+++ model/src/channel/webhook/channel.rs
@@ -0,0 +1,29 @@
+use crate::id::ChannelId;
+use serde::{Deserialize, Serialize};
+
+/// Partial channel object that a webhook is following.
+#[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
+pub struct WebhookChannel {
+    pub id: ChannelId,
+    pub name: String,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::WebhookChannel;
+    use serde::{Deserialize, Serialize};
+    use static_assertions::{assert_fields, assert_impl_all};
+    use std::{fmt::Debug, hash::Hash};
+
+    assert_fields!(WebhookChannel: id, name);
+
+    assert_impl_all!(
+        WebhookChannel: Clone,
+        Debug,
+        Deserialize<'static>,
+        Eq,
+        Hash,
+        PartialEq,
+        Serialize
+    );
+}
