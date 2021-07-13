5e1426e6d0fc8f8e94cf4e2b5dcddce97bc953f8
Zeyla Hellyer <vivian@hellyer.dev>
2021-07-13 17:16:07 -0400

    feat(model): fully support audit logs (#1022)
    Fully support audit logs by creating types for Audit Log Changes and
    updating Audit Log Change Keys with new variants. Changes were generic
    `serde_value::Value`s, which provide no typed information. There are now
    variants with typed `new` and `old` values - checked against the API -
    which may or may not both always be present, or never.
    
    The `twilight_model::guild::audit_log` module is now entirely documented
    and tested as well. This has been manually tested by creating as many
    different audit log changes as could be created in the API and then
    deserializing the past 100 entries.
    
    Closes #77.
    
    Signed-off-by: Zeyla Hellyer <zeyla@hellyer.dev>

--- model/src/guild/audit_log/optional_entry_info.rs
+++ model/src/guild/audit_log/optional_entry_info.rs
@@ -1,23 +1,143 @@
-use crate::id::{ChannelId, GenericId, MessageId, UserId};
+use crate::id::{ChannelId, GenericId, MessageId};
 use serde::{Deserialize, Serialize};
 
+/// Additional information for certain [`AuditLogEventType`]s.
+///
+/// [`AuditLogEventType`]: super::AuditLogEventType
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub struct AuditLogOptionalEntryInfo {
+    /// Channel in which the entities were targeted.
+    ///
+    /// The following events have this option:
+    ///
+    /// - [`AuditLogEventType::MemberMove`]
+    /// - [`AuditLogEventType::MessageDelete`]
+    /// - [`AuditLogEventType::MessagePin`]
+    /// - [`AuditLogEventType::MessageUnpin`]
+    /// - [`AuditLogEventType::StageInstanceCreate`]
+    /// - [`AuditLogEventType::StageInstanceDelete`]
+    /// - [`AuditLogEventType::StageInstanceUpdate`]
+    ///
+    /// [`AuditLogEventType::MemberMove`]: super::AuditLogEventType::MemberMove
+    /// [`AuditLogEventType::MessageDelete`]: super::AuditLogEventType::MessageDelete
+    /// [`AuditLogEventType::MessagePin`]: super::AuditLogEventType::MessagePin
+    /// [`AuditLogEventType::MessageUnpin`]: super::AuditLogEventType::MessageUnpin
+    /// [`AuditLogEventType::StageInstanceCreate`]: super::AuditLogEventType::StageInstanceCreate
+    /// [`AuditLogEventType::StageInstanceDelete`]: super::AuditLogEventType::StageInstanceDelete
+    /// [`AuditLogEventType::StageInstanceUpdate`]: super::AuditLogEventType::StageInstanceUpdate
     #[serde(skip_serializing_if = "Option::is_none")]
     pub channel_id: Option<ChannelId>,
+    /// Number of entities that were targeted.
+    ///
+    /// The following events have this option:
+    ///
+    /// - [`AuditLogEventType::MemberDisconnect`]
+    /// - [`AuditLogEventType::MemberMove`]
+    /// - [`AuditLogEventType::MessageBulkDelete`]
+    /// - [`AuditLogEventType::MessageDelete`]
+    ///
+    /// [`AuditLogEventType::MemberDisconnect`]: super::AuditLogEventType::MemberDisconnect
+    /// [`AuditLogEventType::MemberMove`]: super::AuditLogEventType::MemberMove
+    /// [`AuditLogEventType::MessageBulkDelete`]: super::AuditLogEventType::MessageBulkDelete
+    /// [`AuditLogEventType::MessageDelete`]: super::AuditLogEventType::MessageDelete
     #[serde(skip_serializing_if = "Option::is_none")]
     pub count: Option<String>,
+    /// Specified number of days' worth of inactivity members must have in order
+    /// to be kicked.
+    ///
+    /// The following events have this option:
+    ///
+    /// - [`AuditLogEventType::MemberPrune`]
+    ///
+    /// [`AuditLogEventType::MemberPrune`]: super::AuditLogEventType::MemberPrune
     #[serde(skip_serializing_if = "Option::is_none")]
     pub delete_member_days: Option<String>,
+    /// ID of overwritten entity.
+    ///
+    /// The following events have this option:
+    ///
+    /// - [`AuditLogEventType::ChannelOverwriteCreate`]
+    /// - [`AuditLogEventType::ChannelOverwriteDelete`]
+    /// - [`AuditLogEventType::ChannelOverwriteUpdate`]
+    ///
+    /// [`AuditLogEventType::ChannelOverwriteCreate`]: super::AuditLogEventType::ChannelOverwriteCreate
+    /// [`AuditLogEventType::ChannelOverwriteDelete`]: super::AuditLogEventType::ChannelOverwriteDelete
+    /// [`AuditLogEventType::ChannelOverwriteUpdate`]: super::AuditLogEventType::ChannelOverwriteUpdate
     pub id: Option<GenericId>,
+    /// Type of overwritten entity.
+    ///
+    /// The following events have this option:
+    ///
+    /// - [`AuditLogEventType::ChannelOverwriteCreate`]
+    /// - [`AuditLogEventType::ChannelOverwriteDelete`]
+    /// - [`AuditLogEventType::ChannelOverwriteUpdate`]
+    ///
+    /// [`AuditLogEventType::ChannelOverwriteCreate`]: super::AuditLogEventType::ChannelOverwriteCreate
+    /// [`AuditLogEventType::ChannelOverwriteDelete`]: super::AuditLogEventType::ChannelOverwriteDelete
+    /// [`AuditLogEventType::ChannelOverwriteUpdate`]: super::AuditLogEventType::ChannelOverwriteUpdate
     #[serde(rename = "type", skip_serializing_if = "Option::is_none")]
     pub kind: Option<String>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub member_id: Option<UserId>,
+    /// Number of members removed from a change.
+    ///
+    /// The following events have this option:
+    ///
+    /// - [`AuditLogEventType::MemberPrune`]
+    ///
+    /// [`AuditLogEventType::MemberPrune`]: super::AuditLogEventType::MemberPrune
     #[serde(skip_serializing_if = "Option::is_none")]
     pub members_removed: Option<String>,
+    /// ID of the affected message.
+    ///
+    /// The following events have this option:
+    ///
+    /// - [`AuditLogEventType::MessagePin`]
+    /// - [`AuditLogEventType::MessageUnpin`]
+    ///
+    /// [`AuditLogEventType::MessagePin`]: super::AuditLogEventType::MessagePin
+    /// [`AuditLogEventType::MessageUnpin`]: super::AuditLogEventType::MessageUnpin
     #[serde(skip_serializing_if = "Option::is_none")]
     pub message_id: Option<MessageId>,
+    /// Name of a role.
+    ///
+    /// The following events have this option:
+    ///
+    /// - [`AuditLogEventType::ChannelOverwriteCreate`]
+    /// - [`AuditLogEventType::ChannelOverwriteDelete`]
+    /// - [`AuditLogEventType::ChannelOverwriteUpdate`]
+    ///
+    /// [`AuditLogEventType::ChannelOverwriteCreate`]: super::AuditLogEventType::ChannelOverwriteCreate
+    /// [`AuditLogEventType::ChannelOverwriteDelete`]: super::AuditLogEventType::ChannelOverwriteDelete
+    /// [`AuditLogEventType::ChannelOverwriteUpdate`]: super::AuditLogEventType::ChannelOverwriteUpdate
     #[serde(skip_serializing_if = "Option::is_none")]
     pub role_name: Option<String>,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::AuditLogOptionalEntryInfo;
+    use serde::{Deserialize, Serialize};
+    use static_assertions::{assert_fields, assert_impl_all};
+    use std::{fmt::Debug, hash::Hash};
+
+    assert_fields!(
+        AuditLogOptionalEntryInfo: channel_id,
+        count,
+        delete_member_days,
+        id,
+        kind,
+        members_removed,
+        message_id,
+        role_name
+    );
+    assert_impl_all!(
+        AuditLogOptionalEntryInfo: Clone,
+        Debug,
+        Deserialize<'static>,
+        Eq,
+        Hash,
+        PartialEq,
+        Send,
+        Serialize,
+        Sync
+    );
 }
