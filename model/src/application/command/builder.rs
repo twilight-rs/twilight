4bed43ab38126f9110ce7edfa6ca0d0fe67d7fae
Vilgot Fredenberg <vilgot@fredenberg.xyz>
2021-07-20 13:32:30 +0200

    Add options builder, move to seperate module

---
+++ model/src/application/command/builder.rs
@@ -0,0 +1,561 @@
+//! Create a [`Command`] with a builder.
+
+use super::{
+    BaseCommandOptionData, ChoiceCommandOptionData, Command, CommandOption, CommandOptionChoice,
+    OptionsCommandOptionData,
+};
+use crate::id::{ApplicationId, CommandId, GuildId};
+
+/// Builder to create a [`Command`].
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct CommandBuilder(Command);
+
+impl CommandBuilder {
+    /// The maximum total textual length of the command in UTF-16 code points.
+    ///
+    /// This combines the text of the name, description and value for the command and its subcommands and groups.
+    pub const COMMAND_LENGTH_LIMIT: usize = 4000;
+
+    /// The maximum number of options within a command.
+    pub const COMMAND_OPTIONS_LIMIT: usize = 25;
+
+    /// The maximum number of UTF-16 code points that can be in a description.
+    pub const DESCRIPTION_LENGTH_LIMIT: usize = 100;
+
+    /// The maximum number of UTF-16 code points in a name.
+    pub const NAME_LENGTH_LIMIT: usize = 32;
+
+    /// The maximum number of choices within a option.
+    pub const OPTIONS_CHOICES_LIMIT: usize = 25;
+
+    /// The maximum number of subcommand groups within a command.
+    pub const SUBCOMMAND_GROUP_LIMIT: usize = 25;
+
+    /// The maximum number of subcommands within a subcommand group.
+    pub const SUBCOMMAND_LIMIT: usize = 25;
+
+    /// Creates a new default [`Command`] builder.
+    pub fn new(name: impl Into<String>, description: impl Into<String>) -> Self {
+        Self::_new(name.into(), description.into())
+    }
+
+    const fn _new(name: String, description: String) -> Self {
+        Self(Command {
+            application_id: None,
+            guild_id: None,
+            name,
+            default_permission: None,
+            description,
+            id: None,
+            options: Vec::new(),
+        })
+    }
+
+    /// Consume the builder, returning a [`Command`].
+    #[allow(clippy::missing_const_for_fn)]
+    pub fn build(self) -> Command {
+        self.0
+    }
+
+    /// Sets the application id of the command.
+    ///
+    /// Defaults to [`None`].
+    pub const fn application_id(mut self, application_id: ApplicationId) -> Self {
+        self.0.application_id = Some(application_id);
+
+        self
+    }
+
+    /// Sets the guild id of the command.
+    ///
+    /// Defaults to [`None`].
+    pub const fn guild_id(mut self, guild_id: GuildId) -> Self {
+        self.0.guild_id = Some(guild_id);
+
+        self
+    }
+
+    /// Sets the default permission of the command.
+    ///
+    /// Defaults to [`None`].
+    pub const fn default_permission(mut self, default_permission: bool) -> Self {
+        self.0.default_permission = Some(default_permission);
+
+        self
+    }
+
+    /// Sets the id of the command.
+    ///
+    /// Defaults to [`None`].
+    pub const fn id(mut self, id: CommandId) -> Self {
+        self.0.id = Some(id);
+
+        self
+    }
+
+    /// Adds an option to the command.
+    ///
+    /// Defaults to an empty list.
+    pub fn option(self, option: impl Into<CommandOption>) -> Self {
+        self._option(option.into())
+    }
+
+    fn _option(mut self, option: CommandOption) -> Self {
+        self.0.options.push(option);
+
+        self
+    }
+}
+
+pub struct BooleanBuilder(BaseCommandOptionData);
+
+impl BooleanBuilder {
+    pub fn new(name: impl Into<String>, description: impl Into<String>) -> Self {
+        Self::_new(name.into(), description.into())
+    }
+
+    const fn _new(name: String, description: String) -> Self {
+        Self(BaseCommandOptionData {
+            description,
+            name,
+            required: false,
+        })
+    }
+
+    #[allow(clippy::missing_const_for_fn)]
+    pub fn build(self) -> CommandOption {
+        CommandOption::Boolean(self.0)
+    }
+
+    pub const fn required(mut self, required: bool) -> Self {
+        self.0.required = required;
+
+        self
+    }
+}
+
+impl From<BooleanBuilder> for CommandOption {
+    fn from(builder: BooleanBuilder) -> CommandOption {
+        builder.build()
+    }
+}
+
+pub struct ChannelBuilder(BaseCommandOptionData);
+
+impl ChannelBuilder {
+    pub fn new(name: impl Into<String>, description: impl Into<String>) -> Self {
+        Self::_new(name.into(), description.into())
+    }
+
+    const fn _new(name: String, description: String) -> Self {
+        Self(BaseCommandOptionData {
+            description,
+            name,
+            required: false,
+        })
+    }
+
+    #[allow(clippy::missing_const_for_fn)]
+    pub fn build(self) -> CommandOption {
+        CommandOption::Channel(self.0)
+    }
+
+    pub const fn required(mut self, required: bool) -> Self {
+        self.0.required = required;
+
+        self
+    }
+}
+
+impl From<ChannelBuilder> for CommandOption {
+    fn from(builder: ChannelBuilder) -> CommandOption {
+        builder.build()
+    }
+}
+pub struct IntegerBuilder(ChoiceCommandOptionData);
+
+impl IntegerBuilder {
+    pub fn new(name: impl Into<String>, description: impl Into<String>) -> Self {
+        Self::_new(name.into(), description.into())
+    }
+
+    const fn _new(name: String, description: String) -> Self {
+        Self(ChoiceCommandOptionData {
+            choices: Vec::new(),
+            description,
+            name,
+            required: false,
+        })
+    }
+
+    #[allow(clippy::missing_const_for_fn)]
+    pub fn build(self) -> CommandOption {
+        CommandOption::Integer(self.0)
+    }
+
+    pub fn choice(mut self, choice: CommandOptionChoice) -> Self {
+        self.0.choices.push(choice);
+
+        self
+    }
+
+    pub const fn required(mut self, required: bool) -> Self {
+        self.0.required = required;
+
+        self
+    }
+}
+
+impl From<IntegerBuilder> for CommandOption {
+    fn from(builder: IntegerBuilder) -> CommandOption {
+        builder.build()
+    }
+}
+
+pub struct MentionableBuilder(BaseCommandOptionData);
+
+impl MentionableBuilder {
+    pub fn new(name: impl Into<String>, description: impl Into<String>) -> Self {
+        Self::_new(name.into(), description.into())
+    }
+
+    const fn _new(name: String, description: String) -> Self {
+        Self(BaseCommandOptionData {
+            description,
+            name,
+            required: false,
+        })
+    }
+
+    #[allow(clippy::missing_const_for_fn)]
+    pub fn build(self) -> CommandOption {
+        CommandOption::Mentionable(self.0)
+    }
+
+    pub const fn required(mut self, required: bool) -> Self {
+        self.0.required = required;
+
+        self
+    }
+}
+
+impl From<MentionableBuilder> for CommandOption {
+    fn from(builder: MentionableBuilder) -> CommandOption {
+        builder.build()
+    }
+}
+
+pub struct RoleBuilder(BaseCommandOptionData);
+
+impl RoleBuilder {
+    pub fn new(name: impl Into<String>, description: impl Into<String>) -> Self {
+        Self::_new(name.into(), description.into())
+    }
+
+    const fn _new(name: String, description: String) -> Self {
+        Self(BaseCommandOptionData {
+            description,
+            name,
+            required: false,
+        })
+    }
+
+    #[allow(clippy::missing_const_for_fn)]
+    pub fn build(self) -> CommandOption {
+        CommandOption::Role(self.0)
+    }
+
+    pub const fn required(mut self, required: bool) -> Self {
+        self.0.required = required;
+
+        self
+    }
+}
+
+impl From<RoleBuilder> for CommandOption {
+    fn from(builder: RoleBuilder) -> CommandOption {
+        builder.build()
+    }
+}
+
+pub struct StringBuilder(ChoiceCommandOptionData);
+
+impl StringBuilder {
+    pub fn new(name: impl Into<String>, description: impl Into<String>) -> Self {
+        Self::_new(name.into(), description.into())
+    }
+
+    const fn _new(name: String, description: String) -> Self {
+        Self(ChoiceCommandOptionData {
+            choices: Vec::new(),
+            description,
+            name,
+            required: false,
+        })
+    }
+
+    #[allow(clippy::missing_const_for_fn)]
+    pub fn build(self) -> CommandOption {
+        CommandOption::String(self.0)
+    }
+
+    pub fn choice(mut self, choice: CommandOptionChoice) -> Self {
+        self.0.choices.push(choice);
+
+        self
+    }
+
+    pub const fn required(mut self, required: bool) -> Self {
+        self.0.required = required;
+
+        self
+    }
+}
+
+impl From<StringBuilder> for CommandOption {
+    fn from(builder: StringBuilder) -> CommandOption {
+        builder.build()
+    }
+}
+
+pub struct SubCommandBuilder(OptionsCommandOptionData);
+
+impl SubCommandBuilder {
+    pub fn new(name: impl Into<String>, description: impl Into<String>) -> Self {
+        Self::_new(name.into(), description.into())
+    }
+
+    const fn _new(name: String, description: String) -> Self {
+        Self(OptionsCommandOptionData {
+            description,
+            name,
+            options: Vec::new(),
+            required: false,
+        })
+    }
+
+    #[allow(clippy::missing_const_for_fn)]
+    pub fn build(self) -> CommandOption {
+        CommandOption::SubCommand(self.0)
+    }
+
+    /// Adds an option to the sub command.
+    ///
+    /// Defaults to an empty list.
+    pub fn option(self, option: impl Into<CommandOption>) -> Self {
+        self._option(option.into())
+    }
+
+    fn _option(mut self, option: CommandOption) -> Self {
+        self.0.options.push(option);
+
+        self
+    }
+}
+
+impl From<SubCommandBuilder> for CommandOption {
+    fn from(builder: SubCommandBuilder) -> CommandOption {
+        builder.build()
+    }
+}
+
+pub struct SubCommandGroupBuilder(OptionsCommandOptionData);
+
+impl SubCommandGroupBuilder {
+    pub fn new(name: impl Into<String>, description: impl Into<String>) -> Self {
+        Self::_new(name.into(), description.into())
+    }
+
+    const fn _new(name: String, description: String) -> Self {
+        Self(OptionsCommandOptionData {
+            description,
+            name,
+            options: Vec::new(),
+            required: false,
+        })
+    }
+
+    #[allow(clippy::missing_const_for_fn)]
+    pub fn build(self) -> CommandOption {
+        CommandOption::SubCommandGroup(self.0)
+    }
+
+    /// Adds an option to the sub command group.
+    ///
+    /// Defaults to an empty list.
+    pub fn option(self, option: impl Into<CommandOption>) -> Self {
+        self._option(option.into())
+    }
+
+    fn _option(mut self, option: CommandOption) -> Self {
+        self.0.options.push(option);
+
+        self
+    }
+}
+
+impl From<SubCommandGroupBuilder> for CommandOption {
+    fn from(builder: SubCommandGroupBuilder) -> CommandOption {
+        builder.build()
+    }
+}
+
+pub struct UserBuilder(BaseCommandOptionData);
+
+impl UserBuilder {
+    pub fn new(name: impl Into<String>, description: impl Into<String>) -> Self {
+        Self::_new(name.into(), description.into())
+    }
+
+    const fn _new(name: String, description: String) -> Self {
+        Self(BaseCommandOptionData {
+            description,
+            name,
+            required: false,
+        })
+    }
+
+    #[allow(clippy::missing_const_for_fn)]
+    pub fn build(self) -> CommandOption {
+        CommandOption::User(self.0)
+    }
+
+    pub const fn required(mut self, required: bool) -> Self {
+        self.0.required = required;
+
+        self
+    }
+}
+
+impl From<UserBuilder> for CommandOption {
+    fn from(builder: UserBuilder) -> CommandOption {
+        builder.build()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[allow(clippy::too_many_lines)]
+    fn construct_command_with_builder() {
+        // command from <https://discord.com/developers/docs/interactions/slash-commands#example-walkthrough>
+        let command = CommandBuilder::new("permissions","Get or edit permissions for a user or a role")
+            .option(
+                SubCommandGroupBuilder::new("user", "Get or edit permissions for a user")
+                   .option(
+                        SubCommandBuilder::new("get", "Get permissions for a user")
+                            .option(UserBuilder::new("user", "The user to get").required(true))
+                            .option(ChannelBuilder::new("channel", "The channel permissions to get. If omitted, the guild permissions will be returned")),
+                    )
+                    .option(SubCommandBuilder::new("edit", "Edit permissions for a user")
+                        .option(UserBuilder::new("user", "The user to edit").required(true))
+                        .option(ChannelBuilder::new("channel", "The channel permissions to edit. If omitted, the guild permissions will be edited"))),
+                    )
+            .option(
+                SubCommandGroupBuilder::new("role", "Get or edit permissions for a role")
+                    .option(SubCommandBuilder::new("get", "Get permissions for a role")
+                        .option(RoleBuilder::new("role", "The role to get").required(true))
+                        .option(ChannelBuilder::new("channel", "The channel permissions to get. If omitted, the guild permissions will be returned")))
+                    .option(SubCommandBuilder::new("edit","Edit permissions for a role")
+                        .option(RoleBuilder::new("role", "The role to edit").required(true))
+                        .option(ChannelBuilder::new("channel", "The channel permissions to edit. If omitted, the guild permissions will be edited")),
+                    )
+            )
+            .build();
+        let command_manual = Command {
+            application_id: None,
+            guild_id: None,
+            name: String::from("permissions"),
+            default_permission: None,
+            description: String::from("Get or edit permissions for a user or a role"),
+            id: None,
+            options: vec![
+                CommandOption::SubCommandGroup(OptionsCommandOptionData {
+                    description: String::from("Get or edit permissions for a user"),
+                    name: String::from("user"),
+                    options: vec![
+                        CommandOption::SubCommand(OptionsCommandOptionData {
+                            description: String::from("Get permissions for a user"),
+                            name: String::from("get"),
+                            options: vec![
+                                CommandOption::User(BaseCommandOptionData {
+                                    description: String::from("The user to get"),
+                                    name: String::from("user"),
+                                    required: true,
+                                }),
+                                CommandOption::Channel(BaseCommandOptionData {
+                                    description: String::from("The channel permissions to get. If omitted, the guild permissions will be returned"),
+                                    name: String::from("channel"),
+                                    required: false,
+                                })
+                            ],
+                            required: false,
+                        }),
+                        CommandOption::SubCommand(OptionsCommandOptionData {
+                            description: String::from("Edit permissions for a user"),
+                            name: String::from("edit"),
+                            options: vec![
+                                CommandOption::User(BaseCommandOptionData {
+                                    description: String::from("The user to edit"),
+                                    name: String::from("user"),
+                                    required: true,
+                                }),
+                                CommandOption::Channel(BaseCommandOptionData {
+                                    description: String::from("The channel permissions to edit. If omitted, the guild permissions will be edited"),
+                                    name: String::from("channel"),
+                                    required: false,
+                                })
+                            ],
+                            required: false,
+                        }),
+                    ],
+                    required: false,
+                }),
+                CommandOption::SubCommandGroup(OptionsCommandOptionData {
+                    description: String::from("Get or edit permissions for a role"),
+                    name: String::from("role"),
+                    options: vec![
+                        CommandOption::SubCommand(OptionsCommandOptionData {
+                            description: String::from("Get permissions for a role"),
+                            name: String::from("get"),
+                            options: vec![
+                                CommandOption::Role(BaseCommandOptionData {
+                                    description: String::from("The role to get"),
+                                    name: String::from("role"),
+                                    required: true,
+                                }),
+                                CommandOption::Channel(BaseCommandOptionData {
+                                    description: String::from("The channel permissions to get. If omitted, the guild permissions will be returned"),
+                                    name: String::from("channel"),
+                                    required: false,
+                                })
+                            ],
+                            required: false,
+                        }),
+                        CommandOption::SubCommand(OptionsCommandOptionData {
+                            description: String::from("Edit permissions for a role"),
+                            name: String::from("edit"),
+                            options: vec![
+                                CommandOption::Role(BaseCommandOptionData {
+                                    description: String::from("The role to edit"),
+                                    name: String::from("role"),
+                                    required: true,
+                                }),
+                                CommandOption::Channel(BaseCommandOptionData {
+                                    description: String::from("The channel permissions to edit. If omitted, the guild permissions will be edited"),
+                                    name: String::from("channel"),
+                                    required: false,
+                                })
+                            ],
+                            required: false,
+                        }),
+                    ],
+                    required: false,
+                }),
+            ],
+        };
+        assert_eq!(command, command_manual);
+    }
+}
--- model/src/application/command/mod.rs
+++ model/src/application/command/mod.rs
@@ -1,5 +1,6 @@
 //! Used for building commands to send to Discord.
 
+pub mod builder;
 pub mod permissions;
 
 mod command_type;
@@ -46,74 +47,4 @@ pub struct Command {
     pub kind: CommandType,
     #[serde(default)]
     pub options: Vec<CommandOption>,
-}
-
-/// Builder to create a [`Command`].
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub struct CommandBuilder(Command);
-
-impl CommandBuilder {
-    /// Creates a builder to construct a [`Command`].
-    pub const fn new(name: String, description: String) -> Self {
-        Self(Command {
-            application_id: None,
-            guild_id: None,
-            name,
-            default_permission: None,
-            description,
-            id: None,
-            options: Vec::new(),
-        })
-    }
-
-    /// Consume the builder, returning a [`Command`].
-    #[allow(clippy::missing_const_for_fn)]
-    pub fn build(self) -> Command {
-        self.0
-    }
-
-    /// Sets the application id of the command.
-    ///
-    /// Defaults to [`None`].
-    pub const fn application_id(mut self, application_id: ApplicationId) -> Self {
-        self.0.application_id = Some(application_id);
-
-        self
-    }
-
-    /// Sets the guild id of the command.
-    ///
-    /// Defaults to [`None`].
-    pub const fn guild_id(mut self, guild_id: GuildId) -> Self {
-        self.0.guild_id = Some(guild_id);
-
-        self
-    }
-
-    /// Sets the default permission of the command.
-    ///
-    /// Defaults to [`None`].
-    pub const fn default_permission(mut self, default_permission: bool) -> Self {
-        self.0.default_permission = Some(default_permission);
-
-        self
-    }
-
-    /// Sets the id of the command.
-    ///
-    /// Defaults to [`None`].
-    pub const fn id(mut self, id: CommandId) -> Self {
-        self.0.id = Some(id);
-
-        self
-    }
-
-    /// Sets the options of the command.
-    ///
-    /// Defaults to an empty list.
-    pub fn options(mut self, options: Vec<CommandOption>) -> Self {
-        self.0.options = options;
-
-        self
-    }
 }
