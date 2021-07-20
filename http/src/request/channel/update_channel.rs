2c11b9e5ebe64e2d85c45a2067985545666f0e32
Learath2 <learath2@gmail.com>
2021-07-20 18:00:37 +0200

    Don't forget to add the body (update_channel)

--- http/src/request/channel/update_channel.rs
+++ http/src/request/channel/update_channel.rs
@@ -296,7 +296,7 @@ impl<'a> UpdateChannel<'a> {
     fn start(&mut self) -> Result<(), HttpError> {
         let mut request = Request::builder(Route::UpdateChannel {
             channel_id: self.channel_id.0,
-        });
+        }).json(&self.fields)?;
 
         if let Some(reason) = &self.reason {
             request = request.headers(request::audit_header(reason)?);
