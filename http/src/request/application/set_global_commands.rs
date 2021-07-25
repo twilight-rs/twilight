0c9569cffe46044faf26de5535beb78becd9756e
Vilgot Fredenberg <vilgot@fredenberg.xyz>
2021-07-25 04:41:21 +0200

    enhancement!(http/application): return command (#1037)
    Some HTTP requests returned an empty body in the response, when the API
    response returns a command. To fix this, return
    `twilight_model::application::command::Command` from the following HTTP
    requests:
    
    - `CreateGlobalCommand`
    - `CreateGuildCommand`
    - `SetGlobalCommands`
    - `SetGuildCommands`

--- http/src/request/application/set_global_commands.rs
+++ http/src/request/application/set_global_commands.rs
@@ -2,7 +2,7 @@ use crate::{
     client::Client,
     error::Error,
     request::{Request, RequestBuilder},
-    response::{marker::EmptyBody, ResponseFuture},
+    response::{marker::ListBody, ResponseFuture},
     routing::Route,
 };
 use twilight_model::{application::command::Command, id::ApplicationId};
@@ -41,7 +41,7 @@ impl<'a> SetGlobalCommands<'a> {
     /// Execute the request, returning a future resolving to a [`Response`].
     ///
     /// [`Response`]: crate::response::Response
-    pub fn exec(self) -> ResponseFuture<EmptyBody> {
+    pub fn exec(self) -> ResponseFuture<ListBody<Command>> {
         match self.request() {
             Ok(request) => self.http.request(request),
             Err(source) => ResponseFuture::error(source),
