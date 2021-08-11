34d1231e8f56b7633f9e5a2071d2e33c01907925
Zeyla Hellyer <zeyla@hellyer.dev>
2021-08-11 10:33:47 -0400

    Revert "remove unrelated change"
    This reverts commit 8b4a38bb3812269efa5bf4e70e2981c17a862b8f.

--- gateway/examples/request-members/src/main.rs
+++ gateway/examples/request-members/src/main.rs
@@ -13,7 +13,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     tracing_subscriber::fmt::init();
 
     // to interact with the gateway we first need to connect to it (with a shard or cluster)
-    let (shard, mut events) = Shard::new(env::var("DISCORD_TOKEN")?, Intents::GUILD_MEMBERS);
+    let (shard, mut events) = Shard::new(
+        env::var("DISCORD_TOKEN")?,
+        Intents::GUILD_MEMBERS | Intents::GUILDS,
+    );
     shard.start().await?;
     println!("Created shard");
 
