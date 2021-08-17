71e93995c9d89e0b9ed2c7c137515956bd33f756
Zeyla Hellyer <zeyla@hellyer.dev>
2021-08-17 14:55:49 -0400

    refactor!(http): http ratelimiting header API (#1066)
    Rework the header API by taking an iterator of key-value header pairs
    instead of an `http` crate `HeaderMap`, removing the ratelimit error
    types in favor of header parsing error types, and providing additional
    information about how parsing failed. This also does a general rework of
    the code.

--- http/src/ratelimiting/bucket.rs
+++ http/src/ratelimiting/bucket.rs
@@ -211,24 +211,15 @@ impl BucketQueueTask {
 
     async fn handle_headers(&self, headers: &RatelimitHeaders) {
         let ratelimits = match headers {
-            RatelimitHeaders::GlobalLimited { reset_after } => {
-                self.lock_global(Duration::from_secs(*reset_after)).await;
+            RatelimitHeaders::GlobalLimited(global_limited) => {
+                self.lock_global(Duration::from_secs(global_limited.retry_after()))
+                    .await;
 
                 None
             }
             RatelimitHeaders::None => return,
-            RatelimitHeaders::Present {
-                global,
-                limit,
-                remaining,
-                reset_after,
-                ..
-            } => {
-                if *global {
-                    self.lock_global(Duration::from_secs(*reset_after)).await;
-                }
-
-                Some((*limit, *remaining, *reset_after))
+            RatelimitHeaders::Present(present) => {
+                Some((present.limit(), present.remaining(), present.reset_after()))
             }
         };
 
