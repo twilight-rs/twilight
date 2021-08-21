6b91389d37cc39a736371ffdfb82ea1597376f60
Vilgot Fredenberg <vilgot@fredenberg.xyz>
2021-08-21 15:32:15 +0200

    fix(model): remove `OptionsCommandOptionData::required` (#1108)
    Remove the `required` field from `OptionsCommandOptionData` as it is
    supposed to always be `false`.
    
    This is documented in the API docs:
    
    > Using subcommands or subcommand groups will make your base command
    > unusable. You can't send the base `/permissions` command as a valid
    > command if you also have `/permissions add | remove` as subcommands
    > or subcommand groups
    
    - <https://github.com/discord/discord-api-docs/commit/aa200b537e349fe8940e944979f4080410ad6bc5#diff-07978ef9d619062fb1afbb70ea065124531246743444f4354cc9e6c8879f8780R365-R366>
    
    Closes #1018.

--- model/src/application/command/option.rs
+++ model/src/application/command/option.rs
@@ -53,7 +53,7 @@ impl CommandOption {
 
     pub const fn is_required(&self) -> bool {
         match self {
-            CommandOption::SubCommand(data) | CommandOption::SubCommandGroup(data) => data.required,
+            CommandOption::SubCommand(_) | CommandOption::SubCommandGroup(_) => false,
             CommandOption::String(data)
             | CommandOption::Integer(data)
             | CommandOption::Number(data) => data.required,
@@ -97,7 +97,7 @@ impl Serialize for CommandOption {
                 description: data.description.as_ref(),
                 name: data.name.as_ref(),
                 options: Some(data.options.as_ref()),
-                required: data.required,
+                required: false,
                 kind: self.kind(),
             },
             Self::String(data) | Self::Integer(data) | Self::Number(data) => {
@@ -265,7 +265,6 @@ impl<'de> Visitor<'de> for OptionVisitor {
                     description,
                     name,
                     options,
-                    required,
                 })
             }
             CommandOptionType::SubCommandGroup => {
@@ -275,7 +274,6 @@ impl<'de> Visitor<'de> for OptionVisitor {
                     description,
                     name,
                     options,
-                    required,
                 })
             }
             CommandOptionType::String => CommandOption::String(ChoiceCommandOptionData {
@@ -363,9 +361,6 @@ pub struct OptionsCommandOptionData {
     /// [`SubCommandGroup`]: CommandOptionType::SubCommandGroup
     #[serde(default)]
     pub options: Vec<CommandOption>,
-    /// Whether the option is required to be completed by a user.
-    #[serde(default)]
-    pub required: bool,
 }
 
 /// Data supplied to a [`CommandOption`] of type [`String`] or [`Integer`].
@@ -517,7 +512,6 @@ mod tests {
             description: "ponyville".to_owned(),
             name: "equestria".to_owned(),
             options: Vec::new(),
-            required: false,
         });
 
         serde_test::assert_de_tokens(
@@ -618,9 +612,7 @@ mod tests {
                             required: false,
                         }),
                     ],
-                    required: false,
                 })],
-                required: true,
             })],
         };
 
@@ -658,7 +650,7 @@ mod tests {
                 Token::Seq { len: Some(1) },
                 Token::Struct {
                     name: "CommandOptionEnvelope",
-                    len: 5,
+                    len: 4,
                 },
                 Token::Str("description"),
                 Token::Str("sub group desc"),
@@ -831,8 +823,6 @@ mod tests {
                 Token::U8(1),
                 Token::StructEnd,
                 Token::SeqEnd,
-                Token::Str("required"),
-                Token::Bool(true),
                 Token::Str("type"),
                 Token::U8(2),
                 Token::StructEnd,
