eb7aad9ac83cea14242ce0e079cde75ba966f072
Valdemar Erk <valdemar@erk.io>
2021-10-22 19:29:29 +0200

    Revert "missed one"
    This reverts commit 867ac3aad4e80b3dc5aad2fedb59437c5e9f7277.

--- http/examples/get-message/src/main.rs
+++ http/examples/get-message/src/main.rs
@@ -1,4 +1,5 @@
-use std::{env, error::Error, fs::File, io::Read};
+use futures::future;
+use std::{env, error::Error};
 use twilight_http::Client;
 use twilight_model::id::ChannelId;
 
@@ -8,29 +9,16 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     tracing_subscriber::fmt::init();
 
     let client = Client::new(env::var("DISCORD_TOKEN")?);
-    let channel_id = ChannelId::new(745811002771374151).expect("non zero");
+    let channel_id = ChannelId::new(381_926_291_785_383_946).expect("non zero");
 
-    let mut twilight_movie = File::open("twilight.jpg")?;
-    let mut twilight_sparkle = File::open("twilight-sparkle.png")?;
-
-    let mut tm_vec = Vec::new();
-    let mut ts_vec = Vec::new();
-
-    twilight_movie.read_to_end(&mut tm_vec)?;
-    twilight_sparkle.read_to_end(&mut ts_vec)?;
-
-    let files = [
-        ("twilight-sparkle.png", ts_vec.as_slice()),
-        ("twilight.jpg", tm_vec.as_slice()),
-    ];
-
-    client
-        .create_message(channel_id)
-        .content("Testing")
-        .expect("content not a valid length")
-        .files(&files)
-        .exec()
-        .await?;
+    future::join_all((1u8..=10).map(|x| {
+        client
+            .create_message(channel_id)
+            .content(&format!("Ping #{}", x))
+            .expect("content not a valid length")
+            .exec()
+    }))
+    .await;
 
     let me = client.current_user().exec().await?.model().await?;
     println!("Current user: {}#{}", me.name, me.discriminator);
