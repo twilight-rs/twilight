7cf57130127ed1a5a61773a10c6b1463b8e562fc
Cassandra McCarthy <cassie@7596ff.com>
2021-11-12 18:58:58 -0500

    add rustdoc flags

--- netlify.sh
+++ netlify.sh
@@ -1,6 +1,9 @@
 # Install nightly toolchain
 rustup toolchain install nightly
 
+# Configure rustdoc environment
+export RUSTDOCFLAGS="--cfg docsrs -D broken_intra_doc_links"
+
 # Build crates without examples
 exclude_examples=($(grep -h '^name' examples/**/Cargo.toml | cut -d'"' -f2 | xargs -I '{}' echo '--exclude {}'))
 cargo +nightly doc --workspace --no-deps --features=permission-calculator "${exclude_examples[@]}"
