74c3e23dbffe1afee9b5f0192c54ce18e5967acf
Cassandra McCarthy <cassie@7596ff.com>
2021-11-12 18:45:06 -0500

    actually use nightly to build the docs

--- netlify.sh
+++ netlify.sh
@@ -3,8 +3,8 @@ rustup toolchain install nightly
 
 # Build crates without examples
 exclude_examples=($(grep -h '^name' examples/**/Cargo.toml | cut -d'"' -f2 | xargs -I '{}' echo '--exclude {}'))
-cargo doc --workspace --no-deps --features=permission-calculator "${exclude_examples[@]}"
-cargo doc -p twilight-util --no-deps --all-features
+cargo +nightly doc --workspace --no-deps --features=permission-calculator "${exclude_examples[@]}"
+cargo +nightly doc -p twilight-util --no-deps --all-features
 
 # Prepare docs for publish
 echo '<meta http-equiv="refresh" content="0;url=twilight/index.html">' > target/doc/index.html
