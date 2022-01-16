b2d622afe113f12502e9d974f463ef1b1d27f1e1
ITOH <to@itoh.at>
2022-01-16 12:11:26 +0100

    text(cache): add test for `PresenceUpdate`

--- cache/in-memory/src/event/presence.rs
+++ cache/in-memory/src/event/presence.rs
@@ -40,3 +40,45 @@ impl UpdateCache for PresenceUpdate {
         cache.cache_presence(self.guild_id, presence);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test;
+    use twilight_model::{
+        gateway::{
+            event::Event,
+            presence::{ClientStatus, Status, UserOrId},
+        },
+        id::UserId,
+    };
+
+    #[test]
+    fn test_presence_update() {
+        let cache = InMemoryCache::new();
+
+        let guild_id = GuildId::new(1).expect("non zero");
+        let user_id = UserId::new(1).expect("non zero");
+
+        cache.update(&Event::PresenceUpdate(Box::new(PresenceUpdate {
+            activities: Vec::new(),
+            client_status: ClientStatus {
+                desktop: Some(Status::Online),
+                mobile: None,
+                web: None,
+            },
+            game: None,
+            guild_id,
+            status: Status::Online,
+            user: UserOrId::User(test::user(user_id)),
+        })));
+
+        assert_eq!(1, cache.presences.len());
+        assert_eq!(1, cache.guild_presences.len());
+        assert!(cache
+            .guild_presences
+            .get(&guild_id)
+            .unwrap()
+            .contains(&user_id));
+    }
+}
