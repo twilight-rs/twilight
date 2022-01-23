c6a0ecf163735636018368f408f97786bd6a14f4
Zeyla Hellyer <zeyla@hellyer.dev>
2022-01-23 18:58:09 -0500

    refactor(cache/in-memory!): cached voice state
    We have a type, `model::CachedVoiceState`, that is a slimmed down
    version of a voice state. However, we don't actually use this type
    anywhere -- instead we cache `twilight_model` voice states. This is an
    error from back when the cache was made. This makes the cache use the
    cached voice state type.
    
    Signed-off-by: Zeyla Hellyer <zeyla@hellyer.dev>

--- cache/in-memory/src/model/voice_state.rs
+++ cache/in-memory/src/model/voice_state.rs
@@ -82,6 +82,41 @@ impl CachedVoiceState {
     }
 }
 
+impl From<VoiceState> for CachedVoiceState {
+    fn from(voice_state: VoiceState) -> Self {
+        let VoiceState {
+            channel_id,
+            deaf,
+            guild_id,
+            member: _,
+            mute,
+            self_deaf,
+            self_mute,
+            self_stream,
+            self_video: _,
+            session_id,
+            suppress,
+            token,
+            user_id,
+            request_to_speak_timestamp: _,
+        } = voice_state;
+
+        Self {
+            channel_id,
+            deaf,
+            guild_id,
+            mute,
+            self_deaf,
+            self_mute,
+            self_stream,
+            session_id,
+            suppress,
+            token,
+            user_id,
+        }
+    }
+}
+
 impl PartialEq<VoiceState> for CachedVoiceState {
     fn eq(&self, other: &VoiceState) -> bool {
         self.channel_id == other.channel_id
@@ -95,5 +130,56 @@ impl PartialEq<VoiceState> for CachedVoiceState {
             && self.suppress == other.suppress
             && self.token == other.token
             && self.user_id == other.user_id
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::CachedVoiceState;
+    use twilight_model::{id::Id, voice::VoiceState};
+
+    fn voice_state() -> VoiceState {
+        VoiceState {
+            channel_id: Some(Id::new(1)),
+            deaf: false,
+            guild_id: Some(Id::new(2)),
+            member: None,
+            mute: true,
+            self_deaf: false,
+            self_mute: true,
+            self_stream: false,
+            self_video: true,
+            session_id: "ba8bd70ac7239ffc710e2fc8db52f240".to_owned(),
+            suppress: false,
+            token: None,
+            user_id: Id::new(3),
+            request_to_speak_timestamp: None,
+        }
+    }
+
+    #[test]
+    fn test_eq() {
+        let voice_state = voice_state();
+        let cached = CachedVoiceState::from(voice_state.clone());
+
+        assert_eq!(cached, voice_state);
+    }
+
+    #[test]
+    fn test_getters() {
+        let voice_state = voice_state();
+        let cached = CachedVoiceState::from(voice_state.clone());
+
+        assert_eq!(cached.channel_id(), voice_state.channel_id);
+        assert_eq!(cached.deaf(), voice_state.deaf);
+        assert_eq!(cached.guild_id(), voice_state.guild_id);
+        assert_eq!(cached.mute(), voice_state.mute);
+        assert_eq!(cached.self_deaf(), voice_state.self_deaf);
+        assert_eq!(cached.self_mute(), voice_state.self_mute);
+        assert_eq!(cached.self_stream(), voice_state.self_stream);
+        assert_eq!(cached.session_id(), voice_state.session_id);
+        assert_eq!(cached.suppress(), voice_state.suppress);
+        assert_eq!(cached.token(), voice_state.token.as_deref());
+        assert_eq!(cached.user_id(), voice_state.user_id);
     }
 }
