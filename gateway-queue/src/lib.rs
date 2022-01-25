b202f043035410384780434470379f4a88ecb632
Zeyla Hellyer <zeyla@hellyer.dev>
2022-01-25 10:24:06 -0500

    format

--- gateway-queue/src/lib.rs
+++ gateway-queue/src/lib.rs
@@ -50,7 +50,12 @@ mod large_bot_queue;
 pub use large_bot_queue::LargeBotQueue;
 
 use day_limiter::DayLimiter;
-use std::{fmt::Debug, future::{self, Future}, pin::Pin, time::Duration};
+use std::{
+    fmt::Debug,
+    future::{self, Future},
+    pin::Pin,
+    time::Duration,
+};
 use tokio::{
     sync::{
         mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender},
