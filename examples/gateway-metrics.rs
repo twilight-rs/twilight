b5553f4b75945b38e1d9c6c1570ef985bc5a8f72
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2022-02-08 19:52:12 +0800

    update example for gateway-metrics

--- examples/gateway-metrics.rs
+++ examples/gateway-metrics.rs
@@ -20,7 +20,7 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     // Initialize the tracing subscriber.
     tracing_subscriber::fmt::init();
 
-    let intents = Intents::GUILD_BANS | Intents::GUILD_EMOJIS | Intents::GUILD_MESSAGES;
+    let intents = Intents::GUILD_BANS | Intents::GUILD_EMOJIS_AND_STICKERS | Intents::GUILD_MESSAGES;
     let (cluster, mut events) = Cluster::new(env::var("DISCORD_TOKEN")?, intents).await?;
     println!("Created cluster");
 
