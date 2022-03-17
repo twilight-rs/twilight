802d80a1fffcf652c6ce486ab60effd57329fd25
Vilgot Fredenberg <vilgot@fredenberg.xyz>
2022-03-17 13:16:48 +0100

    refactor: only fetch gateway info once

--- gateway/src/cluster/builder.rs
+++ gateway/src/cluster/builder.rs
@@ -73,22 +73,27 @@ impl ClusterBuilder {
             source: Some(Box::new(err)),
         })?;
 
-        if self.shard.gateway_url.is_none() {
+        let mut shard_scheme = None;
+
+        if self.shard.gateway_url.is_none() || self.shard_scheme.is_none() {
             let gateway = Self::retrieve_connect_info(&self.shard.http_client).await?;
 
-            self = self.gateway_url(Some(gateway.url));
+            if self.shard.gateway_url.is_none() {
+                self = self.gateway_url(Some(gateway.url));
+            }
+
+            if self.shard_scheme.is_none() {
+                shard_scheme = Some(ShardScheme::Range {
+                    from: 0,
+                    to: gateway.shards - 1,
+                    total: gateway.shards,
+                });
+            }
         }
 
-        let shard_scheme = if let Some(scheme) = self.shard_scheme {
-            scheme
-        } else {
-            let gateway = Self::retrieve_connect_info(&self.shard.http_client).await?;
-            ShardScheme::Range {
-                from: 0,
-                to: gateway.shards - 1,
-                total: gateway.shards,
-            }
-        };
+        if let Some(scheme) = self.shard_scheme {
+            shard_scheme = Some(scheme);
+        }
 
         let mut shard_config = self.shard.into_config();
 
@@ -98,7 +103,7 @@ impl ClusterBuilder {
             queue: self.queue,
             resume_sessions: self.resume_sessions,
             shard_presence: self.shard_presence,
-            shard_scheme,
+            shard_scheme: shard_scheme.expect("always set"),
         };
 
         Cluster::new_with_config(config, shard_config).await
