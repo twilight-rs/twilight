16297a742db306c4c51ab0458670f8514bbf13c8
Vilgot Fredenberg <vilgot@fredenberg.xyz>
2022-03-18 21:25:23 +0100

    Add notice on `ClusterBuilder` for its Debug impl

--- gateway/src/cluster/builder.rs
+++ gateway/src/cluster/builder.rs
@@ -37,6 +37,7 @@ use twilight_model::gateway::{
 /// ```
 ///
 /// [`large_threshold`]: Self::large_threshold
+// Remember to sync this with the custom Debug implementation.
 pub struct ClusterBuilder {
     queue: Arc<dyn Queue>,
     resume_sessions: HashMap<u64, ResumeSession>,
