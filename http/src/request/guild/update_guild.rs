549351089e53a0e36f0c9ccaedea2adb5cd80789
Zeyla Hellyer <zeyla@hellyer.dev>
2022-04-02 12:21:53 -0400

    docs(http): update guild community feature notice
    Add a notice to the `UpdateGuild::features` method that adding or
    removing the "COMMUNITY" feature requires the `ADMINISTRATOR`
    permission.
    
    Closes #1662.
    
    Signed-off-by: Zeyla Hellyer <zeyla@hellyer.dev>

--- http/src/request/guild/update_guild.rs
+++ http/src/request/guild/update_guild.rs
@@ -162,6 +162,11 @@ impl<'a> UpdateGuild<'a> {
     }
 
     /// Set the enabled features of the guild.
+    ///
+    /// Attempting to add or remove the "COMMUNITY" feature requires the
+    /// [`Permissions::ADMINISTRATOR`] permission.
+    ///
+    /// [`Permissions::ADMINISTRATOR`]: twilight_model::guild::Permissions::ADMINISTRATOR
     pub const fn features(mut self, features: &'a [&'a str]) -> Self {
         self.fields.features = Some(features);
 
