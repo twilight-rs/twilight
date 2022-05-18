fcc9947b22d0a1cec416e4f1a87198778caf5609
mu-arch <6717350+mu-arch@users.noreply.github.com>
2022-05-18 12:40:02 -0500

    Update attachment.rs
    Add example and clarify the purpose of the id field on Attachment.

--- model/src/http/attachment.rs
+++ model/src/http/attachment.rs
@@ -15,6 +15,7 @@ pub struct Attachment {
 
 impl Attachment {
     /// Create a attachment from a filename and bytes.
+    /// The id field is placeholder that can be any u64 value, such as 0,1,2...
     pub const fn from_bytes(filename: String, file: Vec<u8>, id: u64) -> Self {
         Self {
             description: None,
@@ -23,6 +24,35 @@ impl Attachment {
             id,
         }
     }
+    
+    # Examples
+    ///
+    /// ```no_run
+    /// # #[tokio::main] async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    /// use twilight_http::Client;
+    /// use twilight_model::http::attachment;
+    ///
+    /// let client = Client::new("my token".to_owned());
+    /// let mut attachments: Vec<Attachment> = vec![];
+    ///
+    /// let grocery_list: &str = "Apples/nGrapes/nLemonade";
+    /// let message_content = format!("Here is the grocery list!");
+    ///         
+    /// attachments.push(Attachment::from_bytes(
+    ///    "grocerylist.txt".to_owned(),
+    ///    Vec::from(grocery_list),
+    ///    0
+    /// ));
+    ///
+    /// http.create_message(package)
+    ///    .content(&message_content)?
+    ///    .attachments(&attachments)
+    ///    .exec()
+    ///    .await?;
+    ///
+    ///
+    /// # Ok(()) }
+    /// ```
 
     /// Set the description of a attachment, this is used for alt-text
     /// on Discords end.
