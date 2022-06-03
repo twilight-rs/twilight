twilight-rs/twilight

commit b709c7829dc176f09e2d702f87b08b27923ea384

feat!: prevent optional dependency selection (#1690)
Prevent optional dependencies from being selected as features by using
the new `dep:` keyword in Cargo.toml features.

gateway-queue/Cargo.toml
@@ -27,9 +27,9 @@ static_assertions = { default-features = false, version = "1" }
 
 [features]
 default = ["rustls-native-roots", "tracing"]
-native = ["twilight-http/native"]
-rustls-native-roots = ["twilight-http/rustls-native-roots"]
-rustls-webpki-roots = ["twilight-http/rustls-webpki-roots"]
+native = ["dep:twilight-http", "twilight-http?/native"]
+rustls-native-roots = ["dep:twilight-http", "twilight-http?/rustls-native-roots"]
+rustls-webpki-roots = ["dep:twilight-http", "twilight-http?/rustls-webpki-roots"]
 
 [package.metadata.docs.rs]
 all-features = true

gateway/Cargo.toml
@@ -52,11 +52,11 @@ tokio = { default-features = false, features = ["macros", "rt-multi-thread"], ve
 
 [features]
 default = ["rustls-native-roots", "tracing", "zlib-stock"]
-native = ["native-tls", "twilight-http/native", "twilight-gateway-queue/native", "tokio-tungstenite/native-tls"]
-rustls-native-roots = ["rustls-tls", "rustls-native-certs", "twilight-http/rustls-native-roots", "twilight-gateway-queue/rustls-native-roots", "tokio-tungstenite/rustls-tls-native-roots"]
-rustls-webpki-roots = ["rustls-tls", "webpki-roots", "twilight-http/rustls-webpki-roots", "twilight-gateway-queue/rustls-webpki-roots", "tokio-tungstenite/rustls-tls-webpki-roots"]
-zlib-simd = ["flate2/zlib-ng-compat"]
+native = ["dep:native-tls", "twilight-http/native", "twilight-gateway-queue/native", "tokio-tungstenite/native-tls"]
+rustls-native-roots = ["dep:rustls-tls", "dep:rustls-native-certs", "twilight-http/rustls-native-roots", "twilight-gateway-queue/rustls-native-roots", "tokio-tungstenite/rustls-tls-native-roots"]
+rustls-webpki-roots = ["dep:rustls-tls", "dep:webpki-roots", "twilight-http/rustls-webpki-roots", "twilight-gateway-queue/rustls-webpki-roots", "tokio-tungstenite/rustls-tls-webpki-roots"]
+zlib-simd = ["dep:flate2", "flate2?/zlib-ng-compat"]
 # if the `zlib` feature is enabled anywhere in the dependency tree it will
 # always use stock zlib instead of zlib-ng.
 # https://github.com/rust-lang/libz-sys/blob/main/README.md#zlib-ng
-zlib-stock = ["flate2/zlib"]
+zlib-stock = ["dep:flate2", "flate2?/zlib"]

cache/in-memory/Cargo.toml
@@ -31,7 +31,7 @@ tokio = { default-features = false, features = ["macros", "rt-multi-thread"], ve
 twilight-gateway = { default-features = false, features = ["rustls-native-roots"], path = "../../gateway" }
 
 [features]
-permission-calculator = ["twilight-util"]
+permission-calculator = ["dep:twilight-util"]
 
 [package.metadata.docs.rs]
 all-features = true

http/Cargo.toml
@@ -36,11 +36,11 @@ tracing = { default-features = false, features = ["std", "attributes"], optional
 
 [features]
 default = ["decompression", "rustls-native-roots", "tracing"]
-decompression = ["brotli"]
-native = ["hyper-tls"]
-rustls-native-roots = ["hyper-rustls/native-tokio"]
-rustls-webpki-roots = ["hyper-rustls/webpki-tokio"]
-trust-dns = ["hyper-trust-dns"]
+decompression = ["dep:brotli"]
+native = ["dep:hyper-tls"]
+rustls-native-roots = ["dep:hyper-rustls", "hyper-rustls?/native-tokio"]
+rustls-webpki-roots = ["dep:hyper-rustls", "hyper-rustls?/webpki-tokio"]
+trust-dns = ["dep:hyper-trust-dns"]
 
 [dev-dependencies]
 criterion = { default-features = false, version = "0.3" }

lavalink/Cargo.toml
@@ -38,7 +38,7 @@ twilight-http = { default-features = false, features = ["rustls-native-roots"], 
 
 [features]
 default = ["http-support", "rustls-native-roots", "tracing"]
-http-support = ["percent-encoding"]
+http-support = ["dep:percent-encoding"]
 native = ["tokio-tungstenite/native-tls"]
 rustls-native-roots = ["tokio-tungstenite/rustls-tls-native-roots"]
 rustls-webpki-roots = ["tokio-tungstenite/rustls-tls-webpki-roots"]

util/Cargo.toml
@@ -25,11 +25,10 @@ static_assertions = { default-features = false, version = "1" }
 time = { default-features = false, features = ["formatting"], version = "0.3" }
 
 [features]
-default = []
-builder = ["twilight-model", "twilight-validate"]
-link = ["twilight-model"]
-permission-calculator = ["twilight-model"]
-snowflake = ["twilight-model"]
+builder = ["dep:twilight-model", "dep:twilight-validate"]
+link = ["dep:twilight-model"]
+permission-calculator = ["dep:twilight-model"]
+snowflake = ["dep:twilight-model"]
 full = ["builder", "link", "permission-calculator", "snowflake"]
 
 [package.metadata.docs.rs]

http/src/client/connector.rs
@@ -1,10 +1,13 @@
 //! HTTP connectors with different features.
 
 /// HTTPS connector using `rustls` as a TLS backend.
-#[cfg(feature = "hyper-rustls")]
+#[cfg(any(feature = "rustls-native-roots", feature = "rustls-webpki-roots"))]
 type HttpsConnector<T> = hyper_rustls::HttpsConnector<T>;
 /// HTTPS connector using `hyper-tls` as a TLS backend.
-#[cfg(all(feature = "hyper-tls", not(feature = "hyper-rustls")))]
+#[cfg(all(
+    feature = "native",
+    not(any(feature = "rustls-native-roots", feature = "rustls-webpki-roots"))
+))]
 type HttpsConnector<T> = hyper_tls::HttpsConnector<T>;
 
 /// HTTP connector using `trust-dns` as a DNS backend.
@@ -53,7 +56,7 @@ pub fn create() -> Connector {
         .enable_http2()
         .wrap_connector(connector);
     #[cfg(all(
-        feature = "hyper-tls",
+        feature = "native",
         not(feature = "rustls-native-roots"),
         not(feature = "rustls-webpki-roots")
     ))]

commit f64d5bcc6152a0670bc4731dd668cd3303abb6d7

refactor(gateway-queue)!: only `twilight-http` as a feature
`LargeBotQueue` requires a `Client` from `twilight-http`, meaning that
users have to also depend on `twilight-http`. It makes more sense for
them to specify their TLS requirements there, instead of also in
`gateway-queue`.

Also fixes the README, which errorously mentions the non existant
`twilight-http` feature.

This PR finally fixes all errors to `gateway-queue` introduced in #1690

gateway-queue/Cargo.toml
@@ -26,10 +26,7 @@ twilight-http = { default-features = false, optional = true, path = "../http" }
 static_assertions = { default-features = false, version = "1" }
 
 [features]
-default = ["rustls-native-roots"]
-native = ["dep:twilight-http", "twilight-http?/native"]
-rustls-native-roots = ["dep:twilight-http", "twilight-http?/rustls-native-roots"]
-rustls-webpki-roots = ["dep:twilight-http", "twilight-http?/rustls-webpki-roots"]
+default = ["twilight-http"]
 
 [package.metadata.docs.rs]
 all-features = true

gateway-queue/src/lib.rs
@@ -2,24 +2,12 @@
 #![deny(unsafe_code)]
 #![doc = include_str!("../README.md")]
 
-#[cfg(any(
-    feature = "native",
-    feature = "rustls-native-roots",
-    feature = "rustls-webpki-roots"
-))]
+#[cfg(feature = "twilight-http")]
 mod day_limiter;
-#[cfg(any(
-    feature = "native",
-    feature = "rustls-native-roots",
-    feature = "rustls-webpki-roots"
-))]
+#[cfg(feature = "twilight-http")]
 mod large_bot_queue;
 
-#[cfg(any(
-    feature = "native",
-    feature = "rustls-native-roots",
-    feature = "rustls-webpki-roots"
-))]
+#[cfg(feature = "twilight-http")]
 pub use large_bot_queue::LargeBotQueue;
 
 use std::{

gateway/Cargo.toml
@@ -53,9 +53,9 @@ tokio = { default-features = false, features = ["macros", "rt-multi-thread"], ve
 
 [features]
 default = ["rustls-native-roots", "twilight-http", "zlib-stock"]
-native = ["dep:native-tls", "twilight-http?/native", "twilight-gateway-queue/native", "tokio-tungstenite/native-tls"]
-rustls-native-roots = ["dep:rustls-tls", "dep:rustls-native-certs", "twilight-http?/rustls-native-roots", "twilight-gateway-queue/rustls-native-roots", "tokio-tungstenite/rustls-tls-native-roots"]
-rustls-webpki-roots = ["dep:rustls-tls", "dep:webpki-roots", "twilight-http?/rustls-webpki-roots", "twilight-gateway-queue/rustls-webpki-roots", "tokio-tungstenite/rustls-tls-webpki-roots"]
+native = ["dep:native-tls", "twilight-http?/native", "tokio-tungstenite/native-tls"]
+rustls-native-roots = ["dep:rustls-tls", "dep:rustls-native-certs", "twilight-http?/rustls-native-roots", "tokio-tungstenite/rustls-tls-native-roots"]
+rustls-webpki-roots = ["dep:rustls-tls", "dep:webpki-roots", "twilight-http?/rustls-webpki-roots", "tokio-tungstenite/rustls-tls-webpki-roots"]
 zlib-simd = ["dep:flate2", "flate2?/zlib-ng"]
 zlib-stock = ["dep:flate2", "flate2?/zlib"]
 

twilight/Cargo.toml
@@ -22,7 +22,7 @@ tokio = { default-features = false, features = ["macros", "rt-multi-thread"], ve
 twilight-cache-inmemory = { default-features = false, path = "../cache/in-memory" }
 twilight-embed-builder = { default-features = false, path = "../embed-builder" }
 twilight-gateway = { default-features = false, features = ["rustls-native-roots"], path = "../gateway" }
-twilight-gateway-queue = { default-features = false, features = ["rustls-native-roots"], path = "../gateway-queue" }
+twilight-gateway-queue = { default-features = false, path = "../gateway-queue" }
 twilight-http = { default-features = false, features = ["rustls-native-roots"], path = "../http" }
 twilight-http-ratelimiting = { default-features = false, path = "../http-ratelimiting" }
 twilight-lavalink = { default-features = false, path = "../lavalink" }