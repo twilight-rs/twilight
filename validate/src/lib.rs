e103c08f6d1509caaa50719926f74255f721a94d
Cassandra McCarthy <cassie@7596ff.com>
2022-06-07 15:17:37 -0400

    refactor(validate): standardize clippy lints
    Based on #1644, standardize clippy lints nd fix new ones that appear.

--- validate/src/lib.rs
+++ validate/src/lib.rs
@@ -1,17 +1,22 @@
 #![deny(
     clippy::all,
     clippy::missing_const_for_fn,
-    clippy::missing_docs_in_private_items,
     clippy::pedantic,
     future_incompatible,
     missing_docs,
     nonstandard_style,
     rust_2018_idioms,
+    rustdoc::broken_intra_doc_links,
     unsafe_code,
     unused
 )]
+#![allow(
+    clippy::module_name_repetitions,
+    clippy::must_use_candidate,
+    clippy::unnecessary_wraps,
+    clippy::used_underscore_binding
+)]
 #![doc = include_str!("../README.md")]
-#![allow(clippy::module_name_repetitions)]
 
 pub mod channel;
 pub mod command;
