8eeef4a1dfce51b173895af5043aa8c94313ef10
Cassandra McCarthy <cassie@7596ff.com>
2022-06-08 11:06:19 -0400

    refactor(util): standardize clippy lints (#1786)
    Based on #1644, standardize clippy lints nd fix new ones that appear.

--- util/src/lib.rs
+++ util/src/lib.rs
@@ -1,4 +1,3 @@
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![deny(
     clippy::all,
     clippy::missing_const_for_fn,
@@ -7,11 +6,18 @@
     missing_docs,
     nonstandard_style,
     rust_2018_idioms,
+    rustdoc::broken_intra_doc_links,
     unsafe_code,
     unused
 )]
+#![allow(
+    clippy::module_name_repetitions,
+    clippy::must_use_candidate,
+    clippy::unnecessary_wraps,
+    clippy::used_underscore_binding
+)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 #![doc = include_str!("../README.md")]
-#![allow(clippy::semicolon_if_nothing_returned)]
 
 #[cfg(feature = "builder")]
 pub mod builder;
