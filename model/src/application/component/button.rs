2fb93521e3dc87849d460a50064c79e79f758aa8
Vilgot Fredenberg <vilgot@fredenberg.xyz>
2022-06-17 07:45:42 +0200

    reword internal sync notice

--- model/src/application/component/button.rs
+++ model/src/application/component/button.rs
@@ -36,7 +36,7 @@ pub struct Button {
 /// Refer to [the Discord Docs/Button Object] for additional information.
 ///
 /// [the Discord Docs/Button Object]: https://discord.com/developers/docs/interactions/message-components#button-object-button-styles
-// Keep this is sync with `twilight-validate`!
+// Keep in sync with `twilight-validate`!
 #[derive(Clone, Copy, Debug, Deserialize_repr, Eq, Hash, PartialEq, Serialize_repr)]
 #[non_exhaustive]
 #[repr(u8)]
