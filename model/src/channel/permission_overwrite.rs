1d8d50f01fb6e8d94c826bb1121072ed55425a9d
Vilgot Fredenberg <26655508+vilgotf@users.noreply.github.com>
2022-07-01 11:51:55 +0200

    Update model/src/channel/permission_overwrite.rs
    Co-authored-by: baptiste0928 <22115890+baptiste0928@users.noreply.github.com>

--- model/src/channel/permission_overwrite.rs
+++ model/src/channel/permission_overwrite.rs
@@ -16,7 +16,7 @@ pub struct PermissionOverwrite {
 }
 
 /// Type of a permission overwrite target.
-// Keep in sync with `PermissionCalculator`!
+// Keep in sync with `twilight_util::permission_calculator::PermissionCalculator`!
 #[derive(Clone, Copy, Debug, Deserialize_repr, Eq, Hash, PartialEq, Serialize_repr)]
 #[non_exhaustive]
 #[repr(u8)]
