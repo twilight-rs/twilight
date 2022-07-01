910dccce28f62c4c0627e7c0c5d7b9ad2b1232c2
Vilgot Fredenberg <26655508+vilgotf@users.noreply.github.com>
2022-07-01 20:18:35 +0200

    Update model/src/channel/message/interaction.rs
    Co-authored-by: Cassandra McCarthy <cassie@7596ff.com>

--- model/src/channel/message/interaction.rs
+++ model/src/channel/message/interaction.rs
@@ -6,7 +6,7 @@ use crate::{
 };
 use serde::{Deserialize, Serialize};
 
-/// Response to an interaction without an existing message.
+/// Associated interaction metadata.
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub struct MessageInteraction {
     /// ID of the interaction.
