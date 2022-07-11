23cc7de27d8d434e1e96cf315abf359f60b556cd
Vilgot Fredenberg <26655508+vilgotf@users.noreply.github.com>
2022-07-11 10:22:01 +0200

    move `SlimVoiceServerUpdate` docs into `VoiceServerUpdate`

--- model/src/gateway/payload/incoming/voice_server_update.rs
+++ model/src/gateway/payload/incoming/voice_server_update.rs
@@ -3,7 +3,9 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub struct VoiceServerUpdate {
+    /// Discord voice server endpoint.
     pub endpoint: Option<String>,
     pub guild_id: Id<GuildMarker>,
+    /// Voice authentication token to connect to the Discord voice server.
     pub token: String,
 }
