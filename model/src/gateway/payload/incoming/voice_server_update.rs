0e930498ddb4f30b348aa73bd8ec1c552c42c4ef
Vilgot Fredenberg <26655508+vilgotf@users.noreply.github.com>
2022-07-11 10:11:26 +0200

    guild ID is always sent

--- model/src/gateway/payload/incoming/voice_server_update.rs
+++ model/src/gateway/payload/incoming/voice_server_update.rs
@@ -4,6 +4,6 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub struct VoiceServerUpdate {
     pub endpoint: Option<String>,
-    pub guild_id: Option<Id<GuildMarker>>,
+    pub guild_id: Id<GuildMarker>,
     pub token: String,
 }
