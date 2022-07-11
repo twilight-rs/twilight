adffb3ff5fbb933aab65eea396446f6d9f5eae8f
Vilgot Fredenberg <26655508+vilgotf@users.noreply.github.com>
2022-07-11 10:10:11 +0200

    refactor(model)!: remove undocumented `VoiceServerUpdate::channel_id` field

--- model/src/gateway/payload/incoming/voice_server_update.rs
+++ model/src/gateway/payload/incoming/voice_server_update.rs
@@ -1,12 +1,8 @@
-use crate::id::{
-    marker::{ChannelMarker, GuildMarker},
-    Id,
-};
+use crate::id::{marker::GuildMarker, Id};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub struct VoiceServerUpdate {
-    pub channel_id: Option<Id<ChannelMarker>>,
     pub endpoint: Option<String>,
     pub guild_id: Option<Id<GuildMarker>>,
     pub token: String,
