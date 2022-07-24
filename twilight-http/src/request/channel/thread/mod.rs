80e7d2d2b0173f0b1a437469b2b0200262a72d6b
HTGAzureX1212 <39023054+HTG-YT@users.noreply.github.com>
2022-07-24 11:39:17 +0800

    run cargo fmt

--- twilight-http/src/request/channel/thread/mod.rs
+++ twilight-http/src/request/channel/thread/mod.rs
@@ -14,17 +14,11 @@ mod remove_thread_member;
 mod update_thread;
 
 pub use self::{
-    add_thread_member::AddThreadMember,
-    create_forum_thread::CreateForumThread,
-    create_thread::CreateThread,
-    create_thread_from_message::CreateThreadFromMessage,
+    add_thread_member::AddThreadMember, create_forum_thread::CreateForumThread,
+    create_thread::CreateThread, create_thread_from_message::CreateThreadFromMessage,
     get_joined_private_archived_threads::GetJoinedPrivateArchivedThreads,
     get_private_archived_threads::GetPrivateArchivedThreads,
-    get_public_archived_threads::GetPublicArchivedThreads,
-    get_thread_member::GetThreadMember,
-    get_thread_members::GetThreadMembers,
-    join_thread::JoinThread,
-    leave_thread::LeaveThread,
-    remove_thread_member::RemoveThreadMember,
-    update_thread::UpdateThread,
+    get_public_archived_threads::GetPublicArchivedThreads, get_thread_member::GetThreadMember,
+    get_thread_members::GetThreadMembers, join_thread::JoinThread, leave_thread::LeaveThread,
+    remove_thread_member::RemoveThreadMember, update_thread::UpdateThread,
 };
