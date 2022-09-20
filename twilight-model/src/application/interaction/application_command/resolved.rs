b0b6cfae9c6f00d6287dff07521b594eb70f481d
ITOH <to@itoh.at>
2022-09-20 19:06:19 +0200

    fix(model)!: use `ImageHash` for `InteractionMember::avatar`

--- twilight-model/src/application/interaction/application_command/resolved.rs
+++ twilight-model/src/application/interaction/application_command/resolved.rs
@@ -6,7 +6,7 @@ use crate::{
         Id,
     },
     user::User,
-    util::Timestamp,
+    util::{Timestamp, ImageHash},
 };
 use serde::{Deserialize, Serialize};
 use std::collections::hash_map::HashMap;
@@ -72,7 +72,7 @@ pub struct InteractionChannel {
 pub struct InteractionMember {
     /// Member's guild avatar.
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub avatar: Option<String>,
+    pub avatar: Option<ImageHash>,
     /// If the member is timed out, when the timeout will expire.
     pub communication_disabled_until: Option<Timestamp>,
     /// Member guild join date.
