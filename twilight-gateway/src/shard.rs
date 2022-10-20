0fc2d0c01a2b048e98877b5a296bd995d2a9e2ad
Vilgot Fredenberg <26655508+vilgotf@users.noreply.github.com>
2022-10-20 21:06:43 +0200

    perf(gateway): use `tokio::time::Interval` for heartbeating (#1955)
    Replaces the `TickHeartbeatFuture` future with tokio's `Interval` that
    does the same thing.
    
    The tokio `Interval` struct does not require pinnig to be polled and
    also keeps the `Sleep` around instead of recreating it on every call to
    `NextMessageFuture`, which can be called hundred times per second
    depending on the load of the shard.
    
    Co-authored-by: Cassandra McCarthy <cassie@7596ff.com>

--- twilight-gateway/src/shard.rs
+++ twilight-gateway/src/shard.rs
@@ -73,7 +73,8 @@ use crate::{
 };
 use futures_util::{SinkExt, StreamExt};
 use serde::{de::DeserializeOwned, Deserialize};
-use std::{env::consts::OS, str, time::Duration};
+use std::{env::consts::OS, str};
+use tokio::time::{self, Duration, Instant, Interval, MissedTickBehavior};
 use tokio_tungstenite::tungstenite::Message as TungsteniteMessage;
 use twilight_model::gateway::{
     event::{Event, GatewayEventDeserializer},
@@ -294,7 +295,7 @@ pub struct Shard {
     ///
     /// [`GatewayEvent::Hello`]: twilight_model::gateway::event::GatewayEvent::Hello
     /// [connection]: Self::connection
-    heartbeat_interval: Option<Duration>,
+    heartbeat_interval: Option<Interval>,
     /// ID of the shard.
     id: ShardId,
     /// Recent heartbeat latency statistics.
@@ -522,8 +523,7 @@ impl Shard {
             let future = NextMessageFuture::new(
                 self.user_channel.rx_mut(),
                 self.connection.next(),
-                self.heartbeat_interval,
-                self.latency.sent(),
+                self.heartbeat_interval.as_mut(),
             );
 
             let tungstenite_message = match future.await {
@@ -877,12 +877,21 @@ impl Shard {
             Some(OpCode::Hello) => {
                 let event = Self::parse_event::<Hello>(buffer)?;
                 let interval = event.data.heartbeat_interval;
-                let heartbeat_duration = Duration::from_millis(interval);
-                self.heartbeat_interval = Some(heartbeat_duration);
 
                 if self.config().ratelimit_messages() {
                     self.ratelimiter = Some(CommandRatelimiter::new(interval));
                 }
+
+                let period = Duration::from_millis(interval);
+
+                // First heartbeat should have some jitter, see
+                // https://discord.com/developers/docs/topics/gateway#heartbeat-interval
+                let start = Instant::now() + period.mul_f64(rand::random());
+
+                let mut interval = time::interval_at(start, period);
+                interval.set_missed_tick_behavior(MissedTickBehavior::Delay);
+
+                self.heartbeat_interval = Some(interval);
 
                 self.identify().await.map_err(ProcessError::from_send)?;
             }
