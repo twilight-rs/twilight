eb31c4507ab2ebaa61a07f86bfbb62149ef5917c
Vilgot Fredenberg <26655508+vilgotf@users.noreply.github.com>
2022-10-30 00:12:28 +0200

    fix(model): deserialize `DefaultReaction` correctly

--- twilight-model/src/channel/forum.rs
+++ twilight-model/src/channel/forum.rs
@@ -2,25 +2,58 @@ use crate::id::{
     marker::{EmojiMarker, TagMarker},
     Id,
 };
-use serde::{Deserialize, Serialize};
+use serde::{
+    de::Error as DeError, ser::SerializeStruct, Deserialize, Deserializer, Serialize, Serializer,
+};
 
-#[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
-pub struct DefaultReaction {
-    #[serde(flatten)]
-    emoji: ForumReaction,
-}
-
-#[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
-#[serde(rename_all = "snake_case")]
-pub enum ForumReaction {
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub enum DefaultReaction {
     EmojiId(Id<EmojiMarker>),
     EmojiName(String),
+}
+
+impl<'de> Deserialize<'de> for DefaultReaction {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        let internal = InternalDefaultReaction::deserialize(deserializer)?;
+        match (internal.emoji_id, internal.emoji_name) {
+            (None, Some(name)) => Ok(Self::EmojiName(name)),
+            (Some(id), None) => Ok(Self::EmojiId(id)),
+            _ => Err(DeError::custom("too few or many fields")),
+        }
+    }
+}
+
+impl Serialize for DefaultReaction {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        let mut state = serializer.serialize_struct("DefaultReaction", 2)?;
+
+        match self {
+            DefaultReaction::EmojiId(id) => {
+                state.serialize_field("emoji_id", &Some(id))?;
+                state.serialize_field("emoji_name", &None::<()>)?;
+            }
+            DefaultReaction::EmojiName(name) => {
+                state.serialize_field("emoji_id", &None::<()>)?;
+                state.serialize_field("emoji_name", &Some(name))?;
+            }
+        }
+
+        state.end()
+    }
+}
+
+/// Helper struct for [`DefaultReaction`]'s deserialization implementation.
+#[derive(Deserialize)]
+#[serde(rename = "DefaultReaction")]
+struct InternalDefaultReaction {
+    emoji_id: Option<Id<EmojiMarker>>,
+    emoji_name: Option<String>,
 }
 
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub struct ForumTag {
     #[serde(flatten)]
-    pub emoji: ForumReaction,
+    pub emoji: DefaultReaction,
     pub id: Id<TagMarker>,
     pub moderated: bool,
     pub name: String,
@@ -28,7 +61,7 @@ pub struct ForumTag {
 
 #[cfg(test)]
 mod tests {
-    use super::{DefaultReaction, ForumReaction, ForumTag};
+    use super::{DefaultReaction, ForumTag};
     use crate::id::{
         marker::{EmojiMarker, TagMarker},
         Id,
@@ -40,18 +73,21 @@ mod tests {
 
     #[test]
     fn default_reaction() {
-        let value = DefaultReaction {
-            emoji: ForumReaction::EmojiName("name".into()),
-        };
+        let value = DefaultReaction::EmojiName("name".into());
 
-        // justification for `Token::Map`: https://github.com/serde-rs/serde/issues/1346#issuecomment-451715157
         serde_test::assert_tokens(
             &value,
             &[
-                Token::Map { len: None },
+                Token::Struct {
+                    name: "DefaultReaction",
+                    len: 2,
+                },
+                Token::Str("emoji_id"),
+                Token::None,
                 Token::Str("emoji_name"),
+                Token::Some,
                 Token::Str("name"),
-                Token::MapEnd,
+                Token::StructEnd,
             ],
         );
     }
@@ -59,20 +95,22 @@ mod tests {
     #[test]
     fn forum_tag() {
         let value = ForumTag {
-            emoji: ForumReaction::EmojiId(EMOJI_ID),
+            emoji: DefaultReaction::EmojiId(EMOJI_ID),
             id: TAG_ID,
             moderated: false,
             name: "other".into(),
         };
 
-        // justification for `Token::Map`: https://github.com/serde-rs/serde/issues/1346#issuecomment-451715157
         serde_test::assert_tokens(
             &value,
             &[
                 Token::Map { len: None },
                 Token::Str("emoji_id"),
+                Token::Some,
                 Token::NewtypeStruct { name: "Id" },
                 Token::Str("1"),
+                Token::Str("emoji_name"),
+                Token::None,
                 Token::Str("id"),
                 Token::NewtypeStruct { name: "Id" },
                 Token::Str("2"),
