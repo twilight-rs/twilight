936c53b11e9e6fb5f1131b4de1ff872b9913ad4c
Vilgot Fredenberg <26655508+vilgotf@users.noreply.github.com>
2022-10-30 07:18:59 +0100

    just flatten everything and document invariants

--- twilight-model/src/channel/forum.rs
+++ twilight-model/src/channel/forum.rs
@@ -2,74 +2,54 @@ use crate::id::{
     marker::{EmojiMarker, TagMarker},
     Id,
 };
-use serde::{
-    de::{Deserializer, Error as DeError},
-    ser::{SerializeStruct, Serializer},
-    Deserialize, Serialize,
-};
+use serde::{Deserialize, Serialize};
 
+/// Emoji to use as the default way to react to a forum post.
+///
+/// Exactly one of `emoji_id` and `emoji_name` must be set.
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub struct DefaultReaction {
-    #[serde(flatten)]
-    pub emoji: ForumReaction,
+    /// ID of custom guild emoji.
+    ///
+    /// Conflicts with `emoji_name`.
+    pub emoji_id: Option<Id<EmojiMarker>>,
+    /// Unicode emoji character.
+    ///
+    /// Conflicts with `emoji_id`.
+    pub emoji_name: Option<String>,
 }
 
-#[derive(Clone, Debug, Eq, Hash, PartialEq)]
-pub enum ForumReaction {
-    EmojiId(Id<EmojiMarker>),
-    EmojiName(String),
-}
-
-impl<'de> Deserialize<'de> for ForumReaction {
-    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
-        let internal = InternalForumReaction::deserialize(deserializer)?;
-        match (internal.emoji_id, internal.emoji_name) {
-            (None, Some(name)) => Ok(Self::EmojiName(name)),
-            (Some(id), None) => Ok(Self::EmojiId(id)),
-            _ => Err(DeError::custom("too few or many fields")),
-        }
-    }
-}
-
-impl Serialize for ForumReaction {
-    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
-        let mut state = serializer.serialize_struct("ForumReaction", 2)?;
-
-        match self {
-            Self::EmojiId(id) => {
-                state.serialize_field("emoji_id", &Some(id))?;
-                state.serialize_field("emoji_name", &None::<()>)?;
-            }
-            Self::EmojiName(name) => {
-                state.serialize_field("emoji_id", &None::<()>)?;
-                state.serialize_field("emoji_name", &Some(name))?;
-            }
-        }
-
-        state.end()
-    }
-}
-
-/// Helper struct for [`ForumReaction`]'s deserialization implementation.
-#[derive(Deserialize)]
-#[serde(rename = "ForumReaction")]
-struct InternalForumReaction {
-    emoji_id: Option<Id<EmojiMarker>>,
-    emoji_name: Option<String>,
-}
-
+/// Tag that is able to be applied to a thread in a [`GuildForum`] [`Channel`].
+///
+/// May at most contain one of `emoji_id` and `emoji_name`.
+///
+/// [`Channel`]: super::Channel
+/// [`GuildForum`]: super::ChannelType::GuildForum
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub struct ForumTag {
-    #[serde(flatten)]
-    pub emoji: ForumReaction,
+    /// ID of custom guild emoji.
+    ///
+    /// Conflicts with `emoji_name`.
+    pub emoji_id: Option<Id<EmojiMarker>>,
+    /// Unicode emoji character.
+    ///
+    /// Conflicts with `emoji_name`.
+    pub emoji_name: Option<String>,
+    /// ID of the tag.
     pub id: Id<TagMarker>,
+    /// Whether the tag can only be added or removed by [`Member`]s with the
+    /// [`MANAGE_THREADS`] permission.
+    ///
+    /// [`MANAGE_THREADS`]: crate::guild::Permissions::MANAGE_THREADS
+    /// [`Member`]: crate::guild::Member
     pub moderated: bool,
+    /// Name of the tag (0--20 characters).
     pub name: String,
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{DefaultReaction, ForumReaction, ForumTag};
+    use super::{DefaultReaction, ForumTag};
     use crate::id::{
         marker::{EmojiMarker, TagMarker},
         Id,
@@ -82,38 +62,44 @@ mod tests {
     #[test]
     fn default_reaction() {
         let value = DefaultReaction {
-            emoji: ForumReaction::EmojiName("name".into()),
+            emoji_id: None,
+            emoji_name: Some("name".to_owned()),
         };
 
-        // justification for `Token::Map`: https://github.com/serde-rs/serde/issues/1346#issuecomment-451715157
         serde_test::assert_tokens(
             &value,
             &[
-                Token::Map { len: None },
+                Token::Struct {
+                    name: "DefaultReaction",
+                    len: 2,
+                },
                 Token::Str("emoji_id"),
                 Token::None,
                 Token::Str("emoji_name"),
                 Token::Some,
                 Token::Str("name"),
-                Token::MapEnd,
+                Token::StructEnd,
             ],
         );
     }
 
     #[test]
     fn forum_tag() {
-        let value = ForumTag {
-            emoji: ForumReaction::EmojiId(EMOJI_ID),
+        let with_emoji_id = ForumTag {
+            emoji_id: Some(EMOJI_ID),
+            emoji_name: None,
             id: TAG_ID,
             moderated: false,
             name: "other".into(),
         };
 
-        // justification for `Token::Map`: https://github.com/serde-rs/serde/issues/1346#issuecomment-451715157
         serde_test::assert_tokens(
-            &value,
+            &with_emoji_id,
             &[
-                Token::Map { len: None },
+                Token::Struct {
+                    name: "ForumTag",
+                    len: 5,
+                },
                 Token::Str("emoji_id"),
                 Token::Some,
                 Token::NewtypeStruct { name: "Id" },
@@ -127,7 +113,37 @@ mod tests {
                 Token::Bool(false),
                 Token::Str("name"),
                 Token::Str("other"),
-                Token::MapEnd,
+                Token::StructEnd,
+            ],
+        );
+
+        let without_emoji = ForumTag {
+            emoji_id: None,
+            emoji_name: None,
+            id: TAG_ID,
+            moderated: false,
+            name: "other".into(),
+        };
+
+        serde_test::assert_tokens(
+            &without_emoji,
+            &[
+                Token::Struct {
+                    name: "ForumTag",
+                    len: 5,
+                },
+                Token::Str("emoji_id"),
+                Token::None,
+                Token::Str("emoji_name"),
+                Token::None,
+                Token::Str("id"),
+                Token::NewtypeStruct { name: "Id" },
+                Token::Str("2"),
+                Token::Str("moderated"),
+                Token::Bool(false),
+                Token::Str("name"),
+                Token::Str("other"),
+                Token::StructEnd,
             ],
         );
     }
