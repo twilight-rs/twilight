62316c3ff7e459991b99355027f4e95c90565321
Vilgot Fredenberg <26655508+vilgotf@users.noreply.github.com>
2022-11-29 09:56:04 +0100

    event -> message

--- twilight-gateway/src/stream.rs
+++ twilight-gateway/src/stream.rs
@@ -367,7 +367,7 @@ impl<'a> Stream for ShardEventStream<'a> {
 ///         None => break,
 ///     };
 ///
-///     tracing::debug!(?event, shard = ?shard.id(), "received event");
+///     tracing::debug!(?message, shard = ?shard.id(), "received event");
 /// }
 /// # Ok(()) }
 /// ```
