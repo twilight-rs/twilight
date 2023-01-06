461a01eb1c87bf568571ebec45ce0bfda119d4f2
Zeyla <zeyla@hellyer.dev>
2023-01-06 15:10:35 -0800

    refactor(model): abstract null boolean visitor (#2032)
    Abstract the `RoleTags::premium_subscriber` null boolean visitor to the
    visitor module. Work is being done on adding a new field that functions
    exactly like `RoleTags::premium_subscriber`, where a missing field means
    false and a null value means true. It's best to abstract the visitor
    built for the field so it can be re-used elsewhere.

--- twilight-model/src/guild/role_tags.rs
+++ twilight-model/src/guild/role_tags.rs
@@ -7,51 +7,6 @@ use crate::{
 };
 use serde::{Deserialize, Serialize};
 
-/// The role tags' `premium_subscriber` field is tricky. It's an optional null.
-///
-/// If the field is present, then the value is null, meaning that the role is a
-/// premium subscriber. If the field is not present, it means that the role is
-/// *not* a premium subscriber.
-mod premium_subscriber {
-    use serde::{
-        de::{Deserializer, Error as DeError, Visitor},
-        ser::Serializer,
-    };
-    use std::fmt::{Formatter, Result as FmtResult};
-
-    struct PremiumSubscriberVisitor;
-
-    impl<'de> Visitor<'de> for PremiumSubscriberVisitor {
-        type Value = bool;
-
-        fn expecting(&self, f: &mut Formatter<'_>) -> FmtResult {
-            f.write_str("null")
-        }
-
-        fn visit_none<E: DeError>(self) -> Result<Self::Value, E> {
-            Ok(true)
-        }
-
-        // `visit_none` is used by `serde_json` when a present `null` value is
-        // encountered, but other implementations - such as `simd_json` - may
-        // use `visit_unit` instead.
-        fn visit_unit<E: DeError>(self) -> Result<Self::Value, E> {
-            Ok(true)
-        }
-    }
-
-    // Clippy will say this bool can be taken by value, but we need it to be
-    // passed by reference because that's what serde does.
-    #[allow(clippy::trivially_copy_pass_by_ref)]
-    pub fn serialize<S: Serializer>(_: &bool, serializer: S) -> Result<S::Ok, S::Error> {
-        serializer.serialize_none()
-    }
-
-    pub fn deserialize<'de, D: Deserializer<'de>>(deserializer: D) -> Result<bool, D::Error> {
-        deserializer.deserialize_option(PremiumSubscriberVisitor)
-    }
-}
-
 /// Tags that a [`Role`] has.
 ///
 /// [`Role`]: super::Role
@@ -64,7 +19,11 @@ pub struct RoleTags {
     #[serde(skip_serializing_if = "Option::is_none")]
     pub integration_id: Option<Id<IntegrationMarker>>,
     /// Whether this is the guild's premium subscriber role.
-    #[serde(default, skip_serializing_if = "is_false", with = "premium_subscriber")]
+    #[serde(
+        default,
+        skip_serializing_if = "is_false",
+        with = "crate::visitor::null_boolean"
+    )]
     pub premium_subscriber: bool,
 }
 
