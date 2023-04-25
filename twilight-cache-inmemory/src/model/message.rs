82453daffc45ce0a8322ab18c3705158eef7dcc3
suneettipirneni <suneettipirneni@icloud.com>
2023-04-25 18:55:16 -0400

    remove unrelated changes

--- twilight-cache-inmemory/src/model/message.rs
+++ twilight-cache-inmemory/src/model/message.rs
@@ -79,7 +79,7 @@ impl CachedMessageInteraction {
 /// Represents a cached [`Message`].
 ///
 /// [`Message`]: twilight_model::channel::Message
-#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+#[derive(Clone, Debug, PartialEq, Serialize)]
 pub struct CachedMessage {
     activity: Option<MessageActivity>,
     application: Option<MessageApplication>,
@@ -399,7 +399,6 @@ mod tests {
     assert_impl_all!(
         CachedMessage: Clone,
         Debug,
-        Eq,
         From<Message>,
         PartialEq,
         Send,
