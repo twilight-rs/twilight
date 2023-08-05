f63c2d181f32eb992c05941feb4d5eb81aa8e03c
Jack <40802798+Jack073@users.noreply.github.com>
2023-08-05 12:31:34 +0100

    fix(http): concatenate get invite query string

--- twilight-http/src/routing.rs
+++ twilight-http/src/routing.rs
@@ -2627,6 +2627,10 @@ impl Display for Route<'_> {
                 }
 
                 if *with_expiration {
+                    if *with_counts {
+                        f.write_str("&")?;
+                    }
+
                     f.write_str("with_expiration=true")?;
                 }
 
