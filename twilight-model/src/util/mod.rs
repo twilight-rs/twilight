c269fe569249073ec5ebfd7d5251b52235415d3a
Suneet Tipirneni <77477100+suneettipirneni@users.noreply.github.com>
2023-10-07 10:31:30 -0400

    feat(model, cache): add support for super reactions (#2275)
    Ref:
    - https://github.com/discord/discord-api-docs/pull/6056
    
    This PR also adds a new `HexColor` struct for efficiently storing hex
    strings.

--- twilight-model/src/util/mod.rs
+++ twilight-model/src/util/mod.rs
@@ -1,9 +1,10 @@
 //! Utilities for efficiently parsing and representing data from Discord's API.
 
 pub mod datetime;
+pub mod hex_color;
 pub mod image_hash;
 
-pub use self::{datetime::Timestamp, image_hash::ImageHash};
+pub use self::{datetime::Timestamp, hex_color::HexColor, image_hash::ImageHash};
 
 #[allow(clippy::trivially_copy_pass_by_ref)]
 pub(crate) fn is_false(value: &bool) -> bool {
