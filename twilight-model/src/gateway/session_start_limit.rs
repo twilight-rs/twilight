a4818b6dd348af92d3f9fe46bed49cf5f81fe393
vilgotf <26655508+vilgotf@users.noreply.github.com>
2023-10-14 12:25:48 +0200

    fix(model): bump `SessionStartLimit` field sizes
    The [largest bot](https://gist.github.com/advaith1/451dcbca2d7c3503d4f48d63eb918cb0)
    is in ~21M guilds, meaning that its session start limit := max(2000, (guild_count / 1000) * 3)
    is just under u16::MAX (65535), so to safeguard against overflows we
    should bump the field sizes a step (u16 -> u32).
    I also went ahead and bumped the `max_concurrency` field to u16, size
    it's plausible that this could exceed 255 by the largest bots.

--- twilight-model/src/gateway/session_start_limit.rs
+++ twilight-model/src/gateway/session_start_limit.rs
@@ -10,18 +10,14 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub struct SessionStartLimit {
     /// Maximum number of session that may be started concurrently.
-    pub max_concurrency: u8,
+    pub max_concurrency: u16,
     /// Number of remaining sessions for a given time period.
-    ///
-    /// Max 2000.
-    pub remaining: u16,
+    pub remaining: u32,
     /// Milliseconds until `remaining` resets back to `total`.
     pub reset_after: u64,
     /// Total number of sessions that can be started within the given time
     /// period.
-    ///
-    /// Max 2000.
-    pub total: u16,
+    pub total: u32,
 }
 
 #[cfg(test)]
@@ -46,13 +42,13 @@ mod tests {
                     len: 4,
                 },
                 Token::Str("max_concurrency"),
-                Token::U8(16),
+                Token::U16(16),
                 Token::Str("remaining"),
-                Token::U16(998),
+                Token::U32(998),
                 Token::Str("reset_after"),
                 Token::U64(84_686_789),
                 Token::Str("total"),
-                Token::U16(1_000),
+                Token::U32(1_000),
                 Token::StructEnd,
             ],
         );
