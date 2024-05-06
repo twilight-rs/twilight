9ec62be35c24080c62e455440dd58821375b6955
HTGAzureX1212. <39023054+HTGAzureX1212@users.noreply.github.com>
2024-05-06 08:35:48 +0800

    add codeblock and spoiler format

--- twilight-util/src/format.rs
+++ twilight-util/src/format.rs
@@ -1,39 +1,62 @@
+//! Provides the Format trait for specifying formatting with Discord markdown for strings.
+
+/// Format is a trait specifying formatting with Discord markdown for strings
 pub trait Format {
+    /// Returns the bold formatting for a string.
     fn bold(self) -> Self;
 
+    /// Returns the codeblock formatting for a string.
+    fn codeblock(self, language: &str) -> Self;
+
+    /// Returns the inline code formatting for a string.
     fn inline_code(self) -> Self;
 
+    /// Returns the italic formatting for a string.
     fn italic(self) -> Self;
 
+    /// Returns the relative timestamp formatting for a string.
     fn relative_timestamp(self) -> Self;
 
+    /// Returns the underline formatting for a string.
     fn underline(self) -> Self;
 
+    /// Returns the spoiler formatting for a string.
+    fn spoiler(self) -> Self;
+
+    /// Returns the strikethrough formatting for a string.
     fn strikethrough(self) -> Self;
 }
 
-impl Format for &str {
+impl Format for String {
     fn bold(self) -> Self {
-        concat!("**", self, "**")
+        format!("**{self}**")
+    }
+
+    fn codeblock(self, language: &str) -> Self {
+        format!("```{language}\n{self}```")
     }
 
     fn inline_code(self) -> Self {
-        concat!("`", self, "`")
+        format!("`{self}`")
     }
 
     fn italic(self) -> Self {
-        concat!("*", self, "*")
+        format!("*{self}*")
     }
 
     fn relative_timestamp(self) -> Self {
-        concat!("<t:", self, ":R>")
+        format!("<t:{self}:R>")
     }
 
     fn underline(self) -> Self {
-        concat!("__", self, "__")
+        format!("__{self}__")
+    }
+
+    fn spoiler(self) -> Self {
+        format!("||{self}||")
     }
 
     fn strikethrough(self) -> Self {
-        concat!("~~", self, "~~")
+        format!("~~{self}~~")
     }
 }
