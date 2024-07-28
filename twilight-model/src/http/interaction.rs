51addb8f3117e6f6e62f1da74181577e9b149f5e
Rick Martens <info@rickmartens.nl>
2024-07-28 19:24:08 +0200

    Removed deprecation notice
    For some reason CI Minimal crate version, fails deprecation of a enum member. Can't find a fix for this.

--- twilight-model/src/http/interaction.rs
+++ twilight-model/src/http/interaction.rs
@@ -79,7 +79,6 @@ pub struct InteractionResponseData {
 #[derive(Clone, Copy, Debug, Deserialize_repr, Eq, Hash, PartialEq, Serialize_repr)]
 #[non_exhaustive]
 #[repr(u8)]
-#[allow(deprecated)]
 pub enum InteractionResponseType {
     /// Used when responding to a Ping from Discord.
     Pong = 1,
@@ -107,7 +106,6 @@ pub enum InteractionResponseType {
     /// Please send a [`InteractionResponseType::ChannelMessageWithSource`]
     /// with an [`Button`](crate::channel::message::component::Button) with the style [`ButtonStyle::Premium`](crate::channel::message::component::ButtonStyle)
     /// instead.
-    #[deprecated(note = "This response type has been deprecated by Discord.")]
     PremiumRequired = 10,
 }
 
