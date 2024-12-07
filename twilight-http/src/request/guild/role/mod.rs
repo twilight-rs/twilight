9c6b3856946a0f3cc72490138a7f5235c511af7a
Quang Pham <QuangPhamMailbox@gmail.com>
2024-12-07 00:13:43 -0800

    feat(http): add get guild role endpoint

--- twilight-http/src/request/guild/role/mod.rs
+++ twilight-http/src/request/guild/role/mod.rs
@@ -1,10 +1,11 @@
 mod create_role;
 mod delete_role;
 mod get_guild_roles;
+mod get_role;
 mod update_role;
 mod update_role_positions;
 
 pub use self::{
     create_role::CreateRole, delete_role::DeleteRole, get_guild_roles::GetGuildRoles,
-    update_role::UpdateRole, update_role_positions::UpdateRolePositions,
+    get_role::GetRole, update_role::UpdateRole, update_role_positions::UpdateRolePositions,
 };
