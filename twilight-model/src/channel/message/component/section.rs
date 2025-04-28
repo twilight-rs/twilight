ce25b354089a579a71b0717768410e01f23d03c5
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2025-04-28 09:26:25 +0800

    chore(model): add some basic documentation

--- twilight-model/src/channel/message/component/section.rs
+++ twilight-model/src/channel/message/component/section.rs
@@ -1,11 +1,17 @@
 use super::Component;
 use serde::{Deserialize, Serialize};
 
+/// A component representing a section of a message.
 #[derive(Clone, Debug, Eq, Hash, PartialEq, Deserialize, Serialize)]
 pub struct Section {
+    /// Optional identifier for the section.
     pub id: Option<i32>,
-    // At the moment only TextDisplay works.
+    /// An array of components making up this section.
+    ///
+    /// Currently, only the TextDisplay component is supported here.
     pub components: Vec<Component>,
-    // At the moment this can only be Thumbnail or Button.
+    /// An accessory component for this section.
+    ///
+    /// Currently, only Thumbnail or Button components are supported here.
     pub accessory: Box<Component>,
 }
--- twilight-model/src/channel/message/component/separator.rs
+++ twilight-model/src/channel/message/component/separator.rs
@@ -1,17 +1,26 @@
 use serde::{Deserialize, Serialize};
 
+/// A separator is a layout component that adds vertical padding and
+/// visual division between components.
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub struct Separator {
+    /// Optional identifier for the separator.
     pub id: Option<i32>,
+    /// Whether a visual divider should be shown. Defaults to `true`.
     pub divider: Option<bool>,
+    /// The size of the separator padding. Defaults to `Small`.
     pub spacing: Option<SeparatorSpacingSize>,
 }
 
+/// The size of the separator padding.
 #[derive(Clone, Debug, Eq, Hash, PartialEq, Deserialize, Serialize)]
 #[serde(from = "u8", into = "u8")]
 pub enum SeparatorSpacingSize {
+    /// A small separator padding.
     Small,
+    /// A large separator padding.
     Large,
+    /// An arbitrary separator padding.
     Other(u8),
 }
 
