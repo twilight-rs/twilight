ca65253c5d6675d031002aa49bf3734c5749b5fa
zeonzip <96481337+zeonzip@users.noreply.github.com>
2025-11-01 21:07:18 +0100

    Address review comments

--- twilight-model/src/guild/role_colors.rs
+++ twilight-model/src/guild/role_colors.rs
@@ -3,8 +3,6 @@ use serde::{Deserialize, Serialize};
 #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
 pub struct RoleColors {
     pub primary_color: u32,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub secondary_color: Option<u32>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub tertiary_color: Option<u32>,
 }
