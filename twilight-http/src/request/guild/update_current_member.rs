675482203e50570a60fc6af6ceb9e7b847093c60
zeonzip <96481337+zeonzip@users.noreply.github.com>
2025-11-02 22:06:47 +0100

    add features

--- twilight-http/src/request/guild/update_current_member.rs
+++ twilight-http/src/request/guild/update_current_member.rs
@@ -9,13 +9,20 @@ use serde::Serialize;
 use std::future::IntoFuture;
 use twilight_model::id::{Id, marker::GuildMarker};
 use twilight_validate::request::{
-    ValidationError, audit_reason as validate_audit_reason, nickname as validate_nickname,
+    ValidationError, audit_reason as validate_audit_reason, bio as validate_bio,
+    nickname as validate_nickname,
 };
 
 #[derive(Serialize)]
 struct UpdateCurrentMemberFields<'a> {
     #[serde(skip_serializing_if = "Option::is_none")]
     nick: Option<Nullable<&'a str>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    banner: Option<Nullable<&'a str>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    avatar: Option<Nullable<&'a str>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    bio: Option<Nullable<&'a str>>,
 }
 
 /// Update the user's member in a guild.
@@ -30,14 +37,19 @@ pub struct UpdateCurrentMember<'a> {
 impl<'a> UpdateCurrentMember<'a> {
     pub(crate) const fn new(http: &'a Client, guild_id: Id<GuildMarker>) -> Self {
         Self {
-            fields: Ok(UpdateCurrentMemberFields { nick: None }),
+            fields: Ok(UpdateCurrentMemberFields {
+                nick: None,
+                banner: None,
+                avatar: None,
+                bio: None,
+            }),
             guild_id,
             http,
             reason: Ok(None),
         }
     }
 
-    /// Set the current user's nickname.
+    /// Set the current member's nickname.
     ///
     /// Set to [`None`] to clear the nickname.
     ///
@@ -60,6 +72,57 @@ impl<'a> UpdateCurrentMember<'a> {
             Ok(fields)
         });
 
+        self
+    }
+
+    /// Set the current member's banner.
+    ///
+    /// Set to [`None`] to clear the banner.
+    ///
+    /// Uses the [Image Data URI Scheme](https://discord.com/developers/docs/reference#image-data) for image data.
+    pub const fn banner(mut self, banner: Option<&'a str>) -> Self {
+        if let Ok(fields) = self.fields.as_mut() {
+            fields.banner = Some(Nullable(banner));
+        }
+
+        self
+    }
+
+    /// Set the current member's avatar.
+    ///
+    /// Set to [`None`] to clear the avatar.
+    ///
+    /// Uses the [Image Data URI Scheme](https://discord.com/developers/docs/reference#image-data) for image data.
+    pub const fn avatar(mut self, avatar: Option<&'a str>) -> Self {
+        if let Ok(fields) = self.fields.as_mut() {
+            fields.avatar = Some(Nullable(avatar));
+        }
+
+        self
+    }
+
+    /// Set the current member's bio.
+    ///
+    /// Set to [`None`] to clear the bio.
+    ///
+    /// The minimum length is 1 UTF-16 character and the maximum is 400 UTF-16 characters.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error of type [`Bio`] if the bio length is too
+    /// short or too long.
+    ///
+    /// [`Bio`]: twilight_validate::request::ValidationErrorType::Bio
+    pub fn bio(mut self, bio: Option<&'a str>) -> Self {
+        self.fields = self.fields.and_then(|mut fields| {
+            if let Some(bio) = bio {
+                validate_bio(bio)?;
+            }
+
+            fields.bio = Some(Nullable(bio));
+
+            Ok(fields)
+        });
         self
     }
 }
