0dba1de92d8a15c17f39971f63b571a09e0de68e
SuperYummy <dan_git@hotmail.com>
2026-01-01 09:11:14 +0000

    format

--- twilight-model/src/application/interaction/callback/activity_instance_resource.rs
+++ twilight-model/src/application/interaction/callback/activity_instance_resource.rs
@@ -1,7 +1,7 @@
 use serde::Deserialize;
 
 /// Represents the Activity launched by the interaction.
-/// 
+///
 /// See [Discord Docs/Interaction Callback Activity Instance Resource].
 ///
 /// [Discord Docs/Interaction Callback Activity Instance Resource]: https://discord.com/developers/docs/interactions/receiving-and-responding#interaction-callback-interaction-callback-activity-instance-resource
